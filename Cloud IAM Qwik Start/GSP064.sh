61a6ca37080c7f827d550aecac43ceee6cb83ba8
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-01-14 16:52:26 +0530

    Commit

---
+++ Cloud IAM Qwik Start/GSP064.sh
@@ -0,0 +1,13 @@
+gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
+
+echo "Awesome Lab Solution " > sample.txt
+
+gsutil cp sample.txt gs://$DEVSHELL_PROJECT_ID
+
+gcloud projects remove-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USER_2 \
+  --role=roles/viewer
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member=user:$USER_2 \
+  --role=roles/storage.objectViewer
