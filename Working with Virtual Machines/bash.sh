597ebd07b23dcdbf8f4194455739f7eb0435dcb7
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-02-05 12:34:58 +0530

    Commit

---
+++ Working with Virtual Machines/bash.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+screen -r mcs -X stuff '/save-all\n/save-off\n'
+/usr/bin/gcloud storage cp -R ${BASH_SOURCE%/*}/world gs://${YOUR_BUCKET_NAME}-minecraft-backup/$(date "+%Y%m%d-%H%M%S")-world
+screen -r mcs -X stuff '/save-on\n'
