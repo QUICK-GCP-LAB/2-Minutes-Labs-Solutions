17ce48c41c96b037b2af116491108958f862c773
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-02-05 13:03:00 +0530

    Commit

--- Working with Virtual Machines/VM.sh
+++ Working with Virtual Machines/VM.sh
@@ -91,7 +91,4 @@ gcloud compute ssh mc-server --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 gcloud compute instances add-metadata mc-server \
     --metadata project-id=$DEVSHELL_PROJECT_ID,startup-script-url=https://storage.googleapis.com/cloud-training/archinfra/mcserver/startup.sh,shutdown-script-url=https://storage.googleapis.com/cloud-training/archinfra/mcserver/shutdown.sh \
     --zone=$ZONE
-
-echo "${RED}${BOLD} Congratulations for Completing the Lab !!! ${RESET}"
-
 #-----------------------------------------------------end----------------------------------------------------------#
