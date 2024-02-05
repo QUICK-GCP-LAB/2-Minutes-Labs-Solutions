7d28038e9b10178e0a64c748717cb193e33f060a
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-02-05 15:12:50 +0530

    Commit

---
+++ Cloud Storage Qwik Start - Cloud Console/gsp073.sh
@@ -0,0 +1,43 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud config set compute/region $REGION
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
+
+mv ada.jpg kitten.png
+
+gsutil cp kitten.png gs://$DEVSHELL_PROJECT_ID
+
+gsutil cp -r gs://$DEVSHELL_PROJECT_ID/kitten.png .
+
+gsutil cp gs://$DEVSHELL_PROJECT_ID/kitten.png gs://$DEVSHELL_PROJECT_ID/image-folder/
+
+gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
