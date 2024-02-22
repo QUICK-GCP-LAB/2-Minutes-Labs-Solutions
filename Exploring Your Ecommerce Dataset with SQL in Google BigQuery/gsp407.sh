e0add8742b874fefcf77a980680ed0bd68578ffd
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-02-22 14:58:06 +0530

    Commit

---
+++ Exploring Your Ecommerce Dataset with SQL in Google BigQuery/gsp407.sh
@@ -0,0 +1,182 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT COUNT(*) as num_duplicate_rows, * FROM
+\`data-to-insights.ecommerce.all_sessions_raw\`
+GROUP BY
+fullVisitorId, channelGrouping, time, country, city, totalTransactionRevenue, transactions, timeOnSite, pageviews, sessionQualityDim, date, visitId, type, productRefundAmount, productQuantity, productPrice, productRevenue, productSKU, v2ProductName, v2ProductCategory, productVariant, currencyCode, itemQuantity, itemRevenue, transactionRevenue, transactionId, pageTitle, searchKeyword, pagePathLevel1, eCommerceAction_type, eCommerceAction_step, eCommerceAction_option
+HAVING num_duplicate_rows > 1;
+"
+
+echo "${GREEN}${BOLD}
+
+Task 2. Explore ecommerce data and identify duplicate records Completed
+
+${RESET}"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+# schema: https://support.google.com/analytics/answer/3437719?hl=en
+SELECT
+fullVisitorId, # the unique visitor ID
+visitId, # a visitor can have multiple visits
+date, # session date stored as string YYYYMMDD
+time, # time of the individual site hit  (can be 0 to many per visitor session)
+v2ProductName, # not unique since a product can have variants like Color
+productSKU, # unique for each product
+type, # a visitor can visit Pages and/or can trigger Events (even at the same time)
+eCommerceAction_type, # maps to ‘add to cart', ‘completed checkout'
+eCommerceAction_step,
+eCommerceAction_option,
+  transactionRevenue, # revenue of the order
+  transactionId, # unique identifier for revenue bearing transaction
+COUNT(*) as row_count
+FROM
+\`data-to-insights.ecommerce.all_sessions\`
+GROUP BY 1,2,3 ,4, 5, 6, 7, 8, 9, 10,11,12
+HAVING row_count > 1 # find duplicates
+"
+
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  COUNT(*) AS product_views,
+  COUNT(DISTINCT fullVisitorId) AS unique_visitors
+FROM \`data-to-insights.ecommerce.all_sessions\`;
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  COUNT(DISTINCT fullVisitorId) AS unique_visitors,
+  channelGrouping
+FROM \`data-to-insights.ecommerce.all_sessions\`
+GROUP BY channelGrouping
+ORDER BY channelGrouping DESC;
+"
+
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  (v2ProductName) AS ProductName
+FROM \`data-to-insights.ecommerce.all_sessions\`
+GROUP BY ProductName
+ORDER BY ProductName
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  COUNT(*) AS product_views,
+  (v2ProductName) AS ProductName
+FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE type = 'PAGE'
+GROUP BY v2ProductName
+ORDER BY product_views DESC
+LIMIT 5;
+"
+
+
+
+bq query --use_legacy_sql=false \
+"
+WITH unique_product_views_by_person AS (
+-- find each unique product viewed by each visitor
+SELECT
+ fullVisitorId,
+ (v2ProductName) AS ProductName
+FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE type = 'PAGE'
+GROUP BY fullVisitorId, v2ProductName )
+-- aggregate the top viewed products and sort them
+SELECT
+  COUNT(*) AS unique_view_count,
+  ProductName
+FROM unique_product_views_by_person
+GROUP BY ProductName
+ORDER BY unique_view_count DESC
+LIMIT 5
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  COUNT(*) AS product_views,
+  COUNT(productQuantity) AS orders,
+  SUM(productQuantity) AS quantity_product_ordered,
+  v2ProductName
+FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE type = 'PAGE'
+GROUP BY v2ProductName
+ORDER BY product_views DESC
+LIMIT 5;
+"
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  COUNT(*) AS product_views,
+  COUNT(productQuantity) AS orders,
+  SUM(productQuantity) AS quantity_product_ordered,
+  SUM(productQuantity) / COUNT(productQuantity) AS avg_per_order,
+  (v2ProductName) AS ProductName
+FROM \`data-to-insights.ecommerce.all_sessions\`
+WHERE type = 'PAGE'
+GROUP BY v2ProductName
+ORDER BY product_views DESC
+LIMIT 5;
+"
+
+
+echo "${GREEN}${BOLD}
+
+Task 3. Write basic SQL on ecommerce data Completed
+
+${RESET}"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
+
