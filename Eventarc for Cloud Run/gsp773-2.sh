c3d07584af37fe297a0ee429211550d8ecb09db5
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-02-24 17:26:56 +0530

    Commit

---
+++ Eventarc for Cloud Run/gsp773-2.sh
@@ -0,0 +1,42 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+echo "Hello World" > random.txt
+
+gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
+
+gcloud beta eventarc attributes types describe google.cloud.audit.log.v1.written
+
+gcloud beta eventarc triggers create trigger-auditlog \
+--destination-run-service=${SERVICE_NAME} \
+--matching-criteria="type=google.cloud.audit.log.v1.written" \
+--matching-criteria="serviceName=storage.googleapis.com" \
+--matching-criteria="methodName=storage.objects.create" \
+--service-account=${PROJECT_NUMBER}-compute@developer.gserviceaccount.com
+
+gsutil cp random.txt gs://${BUCKET_NAME}/random.txt
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
