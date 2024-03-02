ddc97d567f50b4f736eeb5f42067acb660f2fa66
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-03-02 18:21:32 +0530

    Commit

---
+++ Google Kubernetes Engine Qwik Start/gsp100.sh
@@ -0,0 +1,41 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud config set compute/zone $ZONE
+
+gcloud container clusters create --machine-type=e2-medium --zone=$ZONE lab-cluster
+
+gcloud container clusters get-credentials lab-cluster
+
+kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:1.0
+
+kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+
+Sleep 70
+
+gcloud container clusters delete lab-cluster
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
