98ea77241c30630bb3c7f97962e8ee11c11388b9
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-03-02 23:34:21 +0530

    Commit

---
+++ Set Up Network and HTTP Load Balancers/gsp007.sh
@@ -0,0 +1,162 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export REGION=${ZONE::-2}
+
+gcloud config set compute/zone $ZONE
+
+  gcloud compute instances create www1 \
+    --zone=$ZONE \
+    --tags=network-lb-tag \
+    --machine-type=e2-small \
+    --image-family=debian-11 \
+    --image-project=debian-cloud \
+    --metadata=startup-script='#!/bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      echo "
+<h3>Web Server: www1</h3>" | tee /var/www/html/index.html'
+
+  gcloud compute instances create www2 \
+    --zone=$ZONE \
+    --tags=network-lb-tag \
+    --machine-type=e2-small \
+    --image-family=debian-11 \
+    --image-project=debian-cloud \
+    --metadata=startup-script='#!/bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      echo "
+<h3>Web Server: www2</h3>" | tee /var/www/html/index.html'
+
+  gcloud compute instances create www3 \
+    --zone=$ZONE  \
+    --tags=network-lb-tag \
+    --machine-type=e2-small \
+    --image-family=debian-11 \
+    --image-project=debian-cloud \
+    --metadata=startup-script='#!/bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      echo "
+<h3>Web Server: www3</h3>" | tee /var/www/html/index.html'
+
+gcloud compute firewall-rules create www-firewall-network-lb \
+    --target-tags network-lb-tag --allow tcp:80
+
+echo "${GREEN}${BOLD}Task 2. Create multiple web server instances Completed${RESET}"
+
+gcloud compute addresses create network-lb-ip-1 \
+  --region $REGION
+
+  gcloud compute target-pools create www-pool \
+  --region $REGION --http-health-check basic-check
+
+  gcloud compute target-pools add-instances www-pool \
+    --instances www1,www2,www3
+
+    gcloud compute forwarding-rules create www-rule \
+    --region  $REGION \
+    --ports 80 \
+    --address network-lb-ip-1 \
+    --target-pool www-pool
+
+    echo "${GREEN}${BOLD}Task 3. Configure the load balancing service Completed${RESET}"
+
+    gcloud compute forwarding-rules describe www-rule --region $REGION
+
+    IPADDRESS=$(gcloud compute forwarding-rules describe www-rule --region $REGION --format="json" | jq -r .IPAddress)
+
+    gcloud compute instance-templates create lb-backend-template \
+   --region=$REGION \
+   --network=default \
+   --subnet=default \
+   --tags=allow-health-check \
+   --machine-type=e2-medium \
+   --image-family=debian-11 \
+   --image-project=debian-cloud \
+   --metadata=startup-script='#!/bin/bash
+     apt-get update
+     apt-get install apache2 -y
+     a2ensite default-ssl
+     a2enmod ssl
+     vm_hostname="$(curl -H "Metadata-Flavor:Google" \
+     http://169.254.169.254/computeMetadata/v1/instance/name)"
+     echo "Page served from: $vm_hostname" | \
+     tee /var/www/html/index.html
+     systemctl restart apache2'
+
+     gcloud compute instance-groups managed create lb-backend-group \
+   --template=lb-backend-template --size=2 --zone=$ZONE
+
+   gcloud compute firewall-rules create fw-allow-health-check \
+  --network=default \
+  --action=allow \
+  --direction=ingress \
+  --source-ranges=130.211.0.0/22,35.191.0.0/16 \
+  --target-tags=allow-health-check \
+  --rules=tcp:80
+
+  gcloud compute addresses create lb-ipv4-1 \
+  --ip-version=IPV4 \
+  --global
+
+  gcloud compute addresses describe lb-ipv4-1 \
+  --format="get(address)" \
+  --global
+
+  gcloud compute health-checks create http http-basic-check \
+  --port 80
+
+  gcloud compute backend-services create web-backend-service \
+  --protocol=HTTP \
+  --port-name=http \
+  --health-checks=http-basic-check \
+  --global
+
+  gcloud compute backend-services add-backend web-backend-service \
+  --instance-group=lb-backend-group \
+  --instance-group-zone=$ZONE \
+  --global
+
+  gcloud compute url-maps create web-map-http \
+    --default-service web-backend-service
+
+    gcloud compute target-http-proxies create http-lb-proxy \
+    --url-map web-map-http
+
+    gcloud compute forwarding-rules create http-content-rule \
+   --address=lb-ipv4-1\
+   --global \
+   --target-http-proxy=http-lb-proxy \
+   --ports=80
+
+   echo "${GREEN}${BOLD}Task 5. Create an HTTP load balancer Completed${RESET}"
+
+   echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
