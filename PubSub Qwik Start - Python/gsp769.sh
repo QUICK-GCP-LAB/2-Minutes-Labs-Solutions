7bf45e2eb973e1d81f97af381bb13739d4624825
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-03-07 18:11:49 +0530

    Commit

---
+++ PubSub Qwik Start - Python/gsp769.sh
@@ -0,0 +1,43 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+sudo apt-get install -y virtualenv
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip install --upgrade google-cloud-pubsub
+
+git clone https://github.com/googleapis/python-pubsub.git
+
+cd python-pubsub/samples/snippets
+
+python publisher.py $GOOGLE_CLOUD_PROJECT create MyTopic
+
+python subscriber.py $GOOGLE_CLOUD_PROJECT create MyTopic MySub
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
