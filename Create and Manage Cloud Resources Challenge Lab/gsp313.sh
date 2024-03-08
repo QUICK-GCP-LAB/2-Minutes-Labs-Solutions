50b413093f6e399ddabd762cc8d4d531bf1af0a6
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-03-09 01:39:46 +0530

    Commit

--- Create and Manage Cloud Resources Challenge Lab/gsp313.sh
+++ Create and Manage Cloud Resources Challenge Lab/gsp313.sh
@@ -98,9 +98,9 @@ gcloud compute forwarding-rules create $FIREWALL \
 
 gcloud compute forwarding-rules list
 
-echo "${RED}${BOLD}Task 3. ${RESET}""${WHITE}${BOLD}Set up an HTTP load balancer${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+echo "${RED}${BOLD}Task 3. ${RESET}""${WHITE}${BOLD}Set up an HTTP load balancer${RESET}""${GREEN}${BOLD}Completed${RESET}"
 
-echo "${YELLOW}${BOLD}Note:${RESET}" "${MAGENTA}${BOLD}You may need to wait for ${RESET}" "${RED}${BOLD}5 to 7 minutes${RESET}""${MAGENTA}${BOLD} to get the score for this task.${RESET}"
+echo "${YELLOW}${BOLD}Note:${RESET}""${MAGENTA}${BOLD}You may need to wait for ${RESET}""${RED}${BOLD}5 to 7 minutes${RESET}""${MAGENTA}${BOLD} to get the score for this task.${RESET}"
 
 echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
 
