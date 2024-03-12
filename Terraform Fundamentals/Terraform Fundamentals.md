fffc5bcf9d90c11f6e96d99b529bf9b80299718d
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-03-12 15:08:48 +0530

    Commit

---
+++ Terraform Fundamentals/Terraform Fundamentals.md
@@ -0,0 +1,23 @@
+# Terraform Fundamentals || [GSP156](https://www.cloudskillsboost.google/focuses/1208?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Exploring%20NCAA%20Data%20with%20BigQuery/gsp160.sh
+
+sudo chmod +x gsp160.sh
+
+./gsp160.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Terraform Fundamentals/gsp156.sh
@@ -0,0 +1,56 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud auth list 
+
+cat > instance.tf <<EOF_END
+resource "google_compute_instance" "terraform" {
+  project      = "$DEVSHELL_PROJECT_ID"
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = "$ZONE"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+EOF_END
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
