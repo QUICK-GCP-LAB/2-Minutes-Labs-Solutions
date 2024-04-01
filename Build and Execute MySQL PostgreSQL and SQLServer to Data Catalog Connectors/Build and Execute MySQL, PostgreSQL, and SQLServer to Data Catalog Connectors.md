fc69df0108674dec9ef96af9c52c4de87afa9e03
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-02 03:12:50 +0530

    Commit

---
+++ Build and Execute MySQL PostgreSQL and SQLServer to Data Catalog Connectors/Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors.md
@@ -0,0 +1,26 @@
+# Build and Execute MySQL, PostgreSQL, and SQLServer to Data Catalog Connectors || [GSP814](https://www.cloudskillsboost.google/focuses/11999?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Develop%20your%20Google%20Cloud%20Network%20Challenge%20Lab/gsp321.sh
+
+sudo chmod +x gsp321.sh
+
+./gsp321.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Build and Execute MySQL PostgreSQL and SQLServer to Data Catalog Connectors/gsp814.sh
@@ -0,0 +1,167 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud services enable datacatalog.googleapis.com
+
+echo "${YELLOW}${BOLD}Task 1. ${RESET}""${WHITE}${BOLD}Enable the Data Catalog API${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gsutil cp gs://spls/gsp814/cloudsql-sqlserver-tooling.zip .
+unzip cloudsql-sqlserver-tooling.zip
+
+cd cloudsql-sqlserver-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-sqlserver-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create sqlserver2dc-credentials \
+--display-name  "Service Account for SQL Server to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "sqlserver2dc-credentials.json" \
+--iam-account "sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-sqlserver-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/sqlserver2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--sqlserver-host=$public_ip_address \
+--sqlserver-user=$username \
+--sqlserver-pass=$password \
+--sqlserver-database=$database
+
+echo "${YELLOW}${BOLD}Task 2. ${RESET}""${WHITE}${BOLD}SQL Server to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+cd
+
+gsutil cp gs://spls/gsp814/cloudsql-postgresql-tooling.zip .
+unzip cloudsql-postgresql-tooling.zip
+
+cd cloudsql-postgresql-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-postgresql-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create postgresql2dc-credentials \
+--display-name  "Service Account for PostgreSQL to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "postgresql2dc-credentials.json" \
+--iam-account "postgresql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:postgresql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-postgresql-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/postgresql2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--postgresql-host=$public_ip_address \
+--postgresql-user=$username \
+--postgresql-pass=$password \
+--postgresql-database=$database
+
+
+echo "${YELLOW}${BOLD}Task 3. ${RESET}""${WHITE}${BOLD}PostgreSQL to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+cd
+
+gsutil cp gs://spls/gsp814/cloudsql-mysql-tooling.zip .
+unzip cloudsql-mysql-tooling.zip
+
+cd cloudsql-mysql-tooling/infrastructure/terraform
+
+sed -i "s/us-central1/$REGION/g" variables.tf
+
+cd ~/cloudsql-mysql-tooling
+bash init-db.sh
+
+gcloud iam service-accounts create mysql2dc-credentials \
+--display-name  "Service Account for MySQL to Data Catalog connector" \
+--project $PROJECT_ID
+
+gcloud iam service-accounts keys create "mysql2dc-credentials.json" \
+--iam-account "mysql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:mysql2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
+--quiet \
+--project $PROJECT_ID \
+--role "roles/datacatalog.admin"
+
+cd infrastructure/terraform/
+
+public_ip_address=$(terraform output -raw public_ip_address)
+username=$(terraform output -raw username)
+password=$(terraform output -raw password)
+database=$(terraform output -raw db_name)
+
+cd ~/cloudsql-mysql-tooling
+
+docker run --rm --tty -v \
+"$PWD":/data mesmacosta/mysql2datacatalog:stable \
+--datacatalog-project-id=$PROJECT_ID \
+--datacatalog-location-id=$REGION \
+--mysql-host=$public_ip_address \
+--mysql-user=$username \
+--mysql-pass=$password \
+--mysql-database=$database
+
+echo "${YELLOW}${BOLD}Task 4. ${RESET}""${WHITE}${BOLD}MySQL to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
