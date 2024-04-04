46c1454543bfd551cf310d6c131ee4db1dc31eeb
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-04 14:50:33 +0530

    Commit

---
+++ Get Started with PubSub Challenge Lab/Get Started with PubSub Challenge Lab form-1.md
@@ -0,0 +1,28 @@
+# Get Started with Pub/Sub: Challenge Lab || [ARC113](https://www.cloudskillsboost.google/focuses/63246?parent=catalog) ||
+
+## Solution [here]()
+
+# For Get Started with Pub/Sub: Challenge Lab `form-1`
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Google%20Cloud%20Packet%20Mirroring%20with%20OpenSource%20IDS/gsp474.sh
+
+sudo chmod +x gsp474.sh
+
+./gsp474.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Get Started with PubSub Challenge Lab/form_1.sh
@@ -0,0 +1,38 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+export MSG_BODY='Hello World!'
+
+gcloud pubsub topics create cloud-pubsub-topic
+
+gcloud pubsub subscriptions create cloud-pubsub-subscription --topic cloud-pubsub-topic
+
+gcloud services enable cloudscheduler.googleapis.com
+
+gcloud scheduler jobs create pubsub cron-scheduler-job --schedule="* * * * *" --location $LOCATION --topic cron-job-pubsub-topic --message-body="$MSG_BODY"
+
+gcloud pubsub subscriptions pull cron-job-pubsub-subscription --limit 5
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
