82280f65d7f19654b187554da24d83192fcc78fd
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-05 16:59:02 +0530

    Commit

---
+++ Get Started with Eventarc Challenge Lab/arc118.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Define color variables
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud pubsub topics create $DEVSHELL_PROJECT_ID-topic
+
+gcloud  pubsub subscriptions create --topic $DEVSHELL_PROJECT_ID-topic $DEVSHELL_PROJECT_ID-topic-sub
+
+gcloud run deploy pubsub-events \
+  --image=gcr.io/cloudrun/hello \
+  --platform=managed \
+  --region=$LOCATION \
+  --allow-unauthenticated
+
+gcloud eventarc triggers create pubsub-events-trigger \
+  --location=$LOCATION \
+  --destination-run-service=pubsub-events \
+  --destination-run-region=$LOCATION \
+  --transport-topic=$DEVSHELL_PROJECT_ID-topic \
+  --event-filters="type=google.cloud.pubsub.topic.v1.messagePublished"
+
+gcloud pubsub topics publish $DEVSHELL_PROJECT_ID-topic \
+  --message="Test message"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
