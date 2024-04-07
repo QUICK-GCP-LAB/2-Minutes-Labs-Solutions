7150e71085710d3bfb553f5c67dcce02377d15e7
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-07 12:51:31 +0530

    Commit

--- Google Cloud SDK Qwik Start - RedhatCentos/gsp122.sh
+++ Google Cloud SDK Qwik Start - RedhatCentos/gsp122.sh
@@ -32,6 +32,8 @@ gcloud compute instances create lol \
 --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD \
 --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=lol,image=projects/centos-cloud/global/images/centos-7-v20231010,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
+sleep 30
+
 gcloud compute ssh lol --zone=$ZONE --quiet --command "# Update YUM with Cloud SDK repo information:
 sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-sdk]
@@ -44,9 +46,6 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
 
-# The indentation for the 2nd line of gpgkey is important.
-
-# Install the Cloud SDK
 sudo yum install google-cloud-sdk -y && gcloud init --console-only"
 
 echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
