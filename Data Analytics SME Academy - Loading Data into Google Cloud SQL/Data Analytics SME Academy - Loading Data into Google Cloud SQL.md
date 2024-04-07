8c46d040de429ec39447db5b9c4fb772a54ded18
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-07 12:25:24 +0530

    Commit

---
+++ Data Analytics SME Academy - Loading Data into Google Cloud SQL/Data Analytics SME Academy - Loading Data into Google Cloud SQL.md
@@ -0,0 +1,26 @@
+# Data Analytics SME Academy - Loading Data into Google Cloud SQL || [GSP196](https://www.cloudskillsboost.google/focuses/1157?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Google%20Cloud%20SDK%20Qwik%20Start%20-%20RedhatCentos/gsp122.sh
+
+sudo chmod +x gsp122.sh
+
+./gsp122.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Data Analytics SME Academy - Loading Data into Google Cloud SQL/gsp196.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud auth list
+
+git clone \
+   https://github.com/GoogleCloudPlatform/data-science-on-gcp/
+
+cd data-science-on-gcp/03_sqlstudio
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+export BUCKET=${PROJECT_ID}-ml
+
+gsutil cp create_table.sql \
+    gs://$BUCKET/create_table.sql
+
+gcloud sql instances create flights \
+    --database-version=POSTGRES_13 --cpu=2 --memory=8GiB \
+    --region=$REGION --root-password=Passw0rd
+
+export ADDRESS=$(curl -s http://ipecho.net/plain)/32
+
+gcloud sql instances patch flights --authorized-networks $ADDRESS --quiet
+
+export INSTANCE_NAME=flights
+export DATABASE_NAME=bts
+export SQL_FILE=create_table.sql
+
+# Create a database
+gcloud sql databases create $DATABASE_NAME --instance=$INSTANCE_NAME
+
+SERVICE_ACCOUNT_EMAIL=$(gcloud sql instances describe $INSTANCE_NAME --format='value(serviceAccountEmailAddress)')
+
+gsutil iam ch serviceAccount:$SERVICE_ACCOUNT_EMAIL:roles/storage.objectViewer gs://$BUCKET
+
+# Import the SQL file into the database
+gcloud sql import sql $INSTANCE_NAME \
+    gs://$BUCKET/$SQL_FILE \
+    --database=$DATABASE_NAME \
+    --quiet
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
