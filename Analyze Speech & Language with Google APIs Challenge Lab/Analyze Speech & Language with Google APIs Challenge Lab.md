090cd1c8597840ba2238a1f8e814096c72d260e1
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-08 16:37:41 +0530

    Commit

---
+++ Analyze Speech & Language with Google APIs Challenge Lab/Analyze Speech & Language with Google APIs Challenge Lab.md
@@ -0,0 +1,26 @@
+# Analyze Speech & Language with Google APIs: Challenge Lab || [ARC130](https://www.cloudskillsboost.google/focuses/63851?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export API_KEY=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Google%20Cloud%20SDK%20Qwik%20Start%20-%20RedhatCentos/gsp122.sh
+
+sudo chmod +x gsp122.sh
+
+./gsp122.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Analyze Speech & Language with Google APIs Challenge Lab/arc114.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+cat > nl_request.json <<EOF_CP
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"With approximately 8.2 million people residing in Boston, the capital city of Massachusetts is one of the largest in the United States."
+  },
+  "encodingType":"UTF8"
+}
+EOF_CP
+
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @nl_request.json > nl_response.json
+
+cat > speech_request.json <<EOF_CP
+{
+  "config": {
+      "encoding":"FLAC",
+      "languageCode": "en-US"
+  },
+  "audio": {
+      "uri":"gs://cloud-samples-tests/speech/brooklyn.flac"
+  }
+}
+EOF_CP
+
+curl -s -X POST -H "Content-Type: application/json" --data-binary @speech_request.json \
+"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > speech_response.json
+
+cat > sentiment_analysis.py <<EOF_CP
+
+import argparse
+
+from google.cloud import language_v1
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print(
+            f"Sentence {index} has a sentiment score of {sentence_sentiment}"
+        )
+
+    print(
+        f"Overall Sentiment: score of {score} with magnitude of {magnitude}"
+    )
+    return 0
+
+
+def analyze(movie_review_filename):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language_v1.LanguageServiceClient()
+
+    with open(movie_review_filename) as review_file:
+        # Instantiates a plain text document.
+        content = review_file.read()
+
+    document = language_v1.Document(
+        content=content, type_=language_v1.Document.Type.PLAIN_TEXT
+    )
+    annotations = client.analyze_sentiment(request={"document": document})
+
+    # Print the results
+    print_result(annotations)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "movie_review_filename",
+        help="The filename of the movie review you'd like to analyze.",
+    )
+    args = parser.parse_args()
+
+    analyze(args.movie_review_filename)
+
+EOF_CP
+
+gsutil cp gs://cloud-samples-tests/natural-language/sentiment-samples.tgz .
+
+gunzip sentiment-samples.tgz
+tar -xvf sentiment-samples.tar
+
+python3 sentiment_analysis.py reviews/bladerunner-pos.txt
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
