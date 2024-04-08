7cd77e2003c64ed078faadb790b92a954a0fc07c
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-08 16:21:44 +0530

    Commit

--- Analyze Sentiment with Natural Language API Challenge Lab/arc130.sh
+++ Analyze Sentiment with Natural Language API Challenge Lab/arc130.sh
@@ -1,30 +1,3 @@
-#!/bin/bash
-# Define color variables
-
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
-
 cat > analyze-request.json <<EOF_END
 {
   "document":{
@@ -53,7 +26,3 @@ curl -s -H "Content-Type: application/json" \
 -H "Authorization: Bearer $(gcloud auth print-access-token)" \
 "https://language.googleapis.com/v1/documents:analyzeEntities" \
 -d @multi-nl-request.json > multi-response.txt
-
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-
-#-----------------------------------------------------end----------------------------------------------------------#
