04e6354719d8fda249cf3efbd16e3f01a01186bf
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-13 19:40:34 +0530

    Commit

---
+++ Predict Soccer Match Outcomes with BigQuery ML Challenge Lab/Predict Soccer Match Outcomes with BigQuery ML Challenge Lab.md
@@ -0,0 +1,42 @@
+# Predict Soccer Match Outcomes with BigQuery ML: Challenge Lab || [GSP374](https://www.cloudskillsboost.google/focuses/37320?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export EVENT_TABLE=
+
+export TAG_TABLE=
+
+export X1=
+
+export Y1=
+
+export X2=
+
+export Y2=
+
+export FUNCTION_1=
+
+export FUNCTION_2=
+
+export MODEL_NAME=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Bracketology%20with%20Google%20Machine%20Learning/gsp461.sh
+
+sudo chmod +x gsp461.sh
+
+./gsp461.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Predict Soccer Match Outcomes with BigQuery ML Challenge Lab/gsp374.sh
@@ -0,0 +1,207 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+
+bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect $DEVSHELL_PROJECT_ID:soccer.$EVENT_TABLE gs://spls/bq-soccer-analytics/events.json
+bq load --source_format=CSV --autodetect $DEVSHELL_PROJECT_ID:soccer.$TAG_TABLE gs://spls/bq-soccer-analytics/tags2name.csv
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.competitions gs://spls/bq-soccer-analytics/competitions.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.matches gs://spls/bq-soccer-analytics/matches.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.teams gs://spls/bq-soccer-analytics/teams.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.players gs://spls/bq-soccer-analytics/players.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON $DEVSHELL_PROJECT_ID:soccer.events gs://spls/bq-soccer-analytics/events.json
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+playerId,
+(Players.firstName || ' ' || Players.lastName) AS playerName,
+COUNT(id) AS numPKAtt,
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)) AS numPKGoals,
+SAFE_DIVIDE(
+SUM(IF(101 IN UNNEST(tags.id), 1, 0)),
+COUNT(id)
+) AS PKSuccessRate
+FROM
+\`soccer.$EVENT_TABLE\` Events
+LEFT JOIN
+\`soccer.players\` Players ON
+Events.playerId = Players.wyId
+WHERE
+eventName = 'Free Kick' AND
+subEventName = 'Penalty'
+GROUP BY
+playerId, playerName
+HAVING
+numPkAtt >= 5
+ORDER BY
+PKSuccessRate DESC, numPKAtt DESC
+"
+
+bq query --use_legacy_sql=false \
+"
+WITH
+Shots AS
+(
+SELECT
+*,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(tags.id)) AS isGoal,
+/* Translate 0-100 (x,y) coordinate-based distances to absolute positions
+using "average" field dimensions of 105x68 before combining in 2D dist calc */
+SQRT(
+POW(
+  (100 - positions[ORDINAL(1)].x) * $X1/$Y1,
+  2) +
+POW(
+  (60 - positions[ORDINAL(1)].y) * $X2/$Y2,
+  2)
+ ) AS shotDistance
+FROM
+\`soccer.$EVENT_TABLE\`
+WHERE
+/* Includes both "open play" & free kick shots (including penalties) */
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+)
+SELECT
+ROUND(shotDistance, 0) AS ShotDistRound0,
+COUNT(*) AS numShots,
+SUM(IF(isGoal, 1, 0)) AS numGoals,
+AVG(IF(isGoal, 1, 0)) AS goalPct
+FROM
+Shots
+WHERE
+shotDistance <= 50
+GROUP BY
+ShotDistRound0
+ORDER BY
+ShotDistRound0
+"
+
+bq query --use_legacy_sql=false \
+"
+CREATE MODEL \`$MODEL_NAME\`
+OPTIONS(
+model_type = 'LOGISTIC_REG',
+input_label_cols = ['isGoal']
+) AS
+SELECT
+Events.subEventName AS shotType,
+/* 101 is known Tag for 'goals' from goals table */
+(101 IN UNNEST(Events.tags.id)) AS isGoal,
+\`$FUNCTION_1\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotDistance,
+\`$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) AS shotAngle
+FROM
+\`soccer.$EVENT_TABLE\` Events
+LEFT JOIN
+\`soccer.matches\` Matches ON
+Events.matchId = Matches.wyId
+LEFT JOIN
+\`soccer.competitions\` Competitions ON
+Matches.competitionId = Competitions.wyId
+WHERE
+/* Filter out World Cup matches for model fitting purposes */
+Competitions.name != 'World Cup' AND
+/* Includes both "open play" & free kick shots (including penalties) */
+(
+eventName = 'Shot' OR
+(eventName = 'Free Kick' AND subEventName IN ('Free kick shot', 'Penalty'))
+) AND
+\`$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+Events.positions[ORDINAL(1)].y) IS NOT NULL
+;
+"
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+predicted_isGoal_probs[ORDINAL(1)].prob AS predictedGoalProb,
+* EXCEPT (predicted_isGoal, predicted_isGoal_probs),
+FROM
+ML.PREDICT(
+MODEL \`$MODEL_NAME\`, 
+(
+ SELECT
+   Events.playerId,
+   (Players.firstName || ' ' || Players.lastName) AS playerName,
+   Teams.name AS teamName,
+   CAST(Matches.dateutc AS DATE) AS matchDate,
+   Matches.label AS match,
+ /* Convert match period and event seconds to minute of match */
+   CAST((CASE
+     WHEN Events.matchPeriod = '1H' THEN 0
+     WHEN Events.matchPeriod = '2H' THEN 45
+     WHEN Events.matchPeriod = 'E1' THEN 90
+     WHEN Events.matchPeriod = 'E2' THEN 105
+     ELSE 120
+     END) +
+     CEILING(Events.eventSec / 60) AS INT64)
+     AS matchMinute,
+   Events.subEventName AS shotType,
+   /* 101 is known Tag for 'goals' from goals table */
+   (101 IN UNNEST(Events.tags.id)) AS isGoal,
+ 
+   \`soccer.$FUNCTION_1\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotDistance,
+   \`soccer.$FUNCTION_2\`(Events.positions[ORDINAL(1)].x,
+       Events.positions[ORDINAL(1)].y) AS shotAngle
+ FROM
+   \`soccer.$EVENT_TABLE\` Events
+ LEFT JOIN
+   \`soccer.matches\` Matches ON
+       Events.matchId = Matches.wyId
+ LEFT JOIN
+   \`soccer.competitions\` Competitions ON
+       Matches.competitionId = Competitions.wyId
+ LEFT JOIN
+   \`soccer.players\` Players ON
+       Events.playerId = Players.wyId
+ LEFT JOIN
+   \`soccer.teams\` Teams ON
+       Events.teamId = Teams.wyId
+ WHERE
+   /* Look only at World Cup matches to apply model */
+   Competitions.name = 'World Cup' AND
+   /* Includes both "open play" & free kick shots (but not penalties) */
+   (
+     eventName = 'Shot' OR
+     (eventName = 'Free Kick' AND subEventName IN ('Free kick shot'))
+   ) AND
+   /* Filter only to goals scored */
+   (101 IN UNNEST(Events.tags.id))
+)
+)
+ORDER BY
+predictedgoalProb
+"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
