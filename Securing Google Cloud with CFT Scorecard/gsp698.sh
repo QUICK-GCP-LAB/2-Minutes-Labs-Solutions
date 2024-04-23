b65bcc2158eee18eef9f21a901caaff1dd40df3f
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-23 18:26:20 +0530

    Commit

---
+++ Securing Google Cloud with CFT Scorecard/gsp698.sh
@@ -0,0 +1,70 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export GOOGLE_PROJECT=$DEVSHELL_PROJECT_ID
+
+export CAI_BUCKET_NAME=cai-$GOOGLE_PROJECT
+
+gcloud services enable cloudasset.googleapis.com \
+    --project $GOOGLE_PROJECT
+
+gcloud beta services identity create --service=cloudasset.googleapis.com --project=$GOOGLE_PROJECT
+
+gcloud projects add-iam-policy-binding ${GOOGLE_PROJECT}  \
+   --member=serviceAccount:service-$(gcloud projects list --filter="$GOOGLE_PROJECT" --format="value(PROJECT_NUMBER)")@gcp-sa-cloudasset.iam.gserviceaccount.com \
+   --role=roles/storage.admin
+
+git clone https://github.com/forseti-security/policy-library.git
+
+cp policy-library/samples/storage_denylist_public.yaml policy-library/policies/constraints/
+
+gsutil mb -l $REGION -p $GOOGLE_PROJECT gs://$CAI_BUCKET_NAME
+
+# Export resource data
+gcloud asset export \
+    --output-path=gs://$CAI_BUCKET_NAME/resource_inventory.json \
+    --content-type=resource \
+    --project=$GOOGLE_PROJECT
+
+# Export IAM data
+gcloud asset export \
+    --output-path=gs://$CAI_BUCKET_NAME/iam_inventory.json \
+    --content-type=iam-policy \
+    --project=$GOOGLE_PROJECT
+
+# Export org policy data
+gcloud asset export \
+    --output-path=gs://$CAI_BUCKET_NAME/org_policy_inventory.json \
+    --content-type=org-policy \
+    --project=$GOOGLE_PROJECT
+
+# Export access policy data
+gcloud asset export \
+    --output-path=gs://$CAI_BUCKET_NAME/access_policy_inventory.json \
+    --content-type=access-policy \
+    --project=$GOOGLE_PROJECT
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
