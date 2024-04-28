d09288384372125b5ab207a250e66d2f9593d9a6
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-28 10:10:04 +0530

    Commit

--- Manage Bigtable on Google Cloud Challenge Lab/gsp380.sh
+++ Manage Bigtable on Google Cloud Challenge Lab/gsp380.sh
@@ -83,6 +83,8 @@ gcloud dataflow jobs run import-sessions --gcs-location gs://dataflow-templates-
 
 gcloud dataflow jobs run import-recommendations --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=PersonalizedProducts,sourcePattern=gs://cloud-training/OCBL377/retail-recommendations-00000-of-00001
 
+echo "${RED}${BOLD}Check ${RESET}""${WHITE}${BOLD}Task 4${RESET}" "${GREEN}${BOLD}Score${RESET}"
+
 sleep 120
 
 gcloud bigtable backups delete PersonalizedProducts_7 --instance=ecommerce-recommendations \
