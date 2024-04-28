4cda259c1f688ca300e73731a96804a1ebcac75b
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-28 12:24:04 +0530

    Commit

---
+++ Use APIs to Work with Cloud Storage Challenge Lab/Use APIs to Work with Cloud Storage Challenge Lab.md
@@ -0,0 +1,23 @@
+# Use APIs to Work with Cloud Storage: Challenge Lab || [ARC125](https://www.cloudskillsboost.google/focuses/65991?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Streaming%20Analytics%20into%20BigQuery%20Challenge%20Lab/arc106.sh
+
+sudo chmod +x arc106.sh
+
+./arc106.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Use APIs to Work with Cloud Storage Challenge Lab/arc125.sh
@@ -0,0 +1,73 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+# Create bucket1.json
+cat > bucket1.json <<EOF
+{  
+   "name": "$DEVSHELL_PROJECT_ID-bucket-1",
+   "location": "us",
+   "storageClass": "multi_regional"
+}
+EOF
+
+# Create bucket1
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data-binary @bucket1.json "https://storage.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+
+# Create bucket2.json
+cat > bucket2.json <<EOF
+{  
+   "name": "$DEVSHELL_PROJECT_ID-bucket-2",
+   "location": "us",
+   "storageClass": "multi_regional"
+}
+EOF
+
+# Create bucket2
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data-binary @bucket2.json "https://storage.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+
+# Upload image file to bucket1
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: image/jpeg" --data-binary @world.jpeg "https://storage.googleapis.com/upload/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o?uploadType=media&name=quicklab.jpeg"
+
+# Copy file from bucket1 to bucket2
+curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" --data '{"destination": "$DEVSHELL_PROJECT_ID-bucket-2"}' "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg/copyTo/b/$DEVSHELL_PROJECT_ID-bucket-2/o/quicklab.jpeg"
+
+# Make the object publicly accessible
+cat > public_access.json <<EOF
+{
+  "entity": "allUsers",
+  "role": "READER"
+}
+EOF
+
+curl -X POST --data-binary @public_access.json -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg/acl"
+
+# Delete the object
+curl -X DELETE -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1/o/quicklab.jpeg"
+
+# Delete bucket1
+curl -X DELETE -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://storage.googleapis.com/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket-1"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
