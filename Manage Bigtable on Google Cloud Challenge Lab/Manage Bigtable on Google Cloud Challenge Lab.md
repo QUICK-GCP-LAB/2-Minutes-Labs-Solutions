f4ad51daffc74cf679a599a97f209a7ae5455490
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-04-28 10:06:21 +0530

    Commit

---
+++ Manage Bigtable on Google Cloud Challenge Lab/Manage Bigtable on Google Cloud Challenge Lab.md
@@ -0,0 +1,28 @@
+# Manage Bigtable on Google Cloud: Challenge Lab || [GSP380](https://www.cloudskillsboost.google/focuses/62217?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+### Assign Veriables
+```
+export ZONE=
+export ZONE2=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Store%2C%20Process%2C%20and%20Manage%20Data%20on%20Google%20Cloud%20Challenge%20Lab/arc100.sh
+
+sudo chmod +x arc100.sh
+
+./arc100.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Manage Bigtable on Google Cloud Challenge Lab/gsp380.sh
@@ -0,0 +1,88 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export REGION="${ZONE%-*}"
+
+gcloud services disable dataflow.googleapis.com
+
+gcloud services enable dataflow.googleapis.com
+
+gcloud bigtable instances create ecommerce-recommendations \
+  --display-name=ecommerce-recommendations \
+  --cluster-storage-type=SSD \
+  --cluster-config="id=ecommerce-recommendations-c1,zone=$ZONE"
+
+gcloud bigtable clusters update ecommerce-recommendations-c1 \
+    --instance=ecommerce-recommendations \
+    --autoscaling-max-nodes=5 \
+    --autoscaling-min-nodes=1 \
+    --autoscaling-cpu-target=60 
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+gcloud bigtable instances tables create SessionHistory \
+    --instance=ecommerce-recommendations \
+    --project=$DEVSHELL_PROJECT_ID \
+    --column-families=Engagements,Sales
+
+gcloud bigtable instances tables create PersonalizedProducts \
+    --instance=ecommerce-recommendations \
+    --project=$DEVSHELL_PROJECT_ID \
+    --column-families=Recommendations
+
+gcloud dataflow jobs run import-sessions --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=SessionHistory,sourcePattern=gs://cloud-training/OCBL377/retail-engagements-sales-00000-of-00001,mutationThrottleLatencyMs=0
+
+gcloud dataflow jobs run import-recommendations --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=PersonalizedProducts,sourcePattern=gs://cloud-training/OCBL377/retail-recommendations-00000-of-00001
+
+gcloud bigtable clusters create ecommerce-recommendations-c2 \
+    --instance=ecommerce-recommendations \
+    --zone=$ZONE2
+
+gcloud bigtable clusters update ecommerce-recommendations-c2 \
+    --instance=ecommerce-recommendations \
+    --autoscaling-max-nodes=5 \
+    --autoscaling-min-nodes=1 \
+    --autoscaling-cpu-target=60 
+
+gcloud bigtable backups create PersonalizedProducts_7 --instance=ecommerce-recommendations \
+  --cluster=ecommerce-recommendations-c1 \
+  --table=PersonalizedProducts \
+  --retention-period=7d 
+
+gcloud bigtable instances tables restore \
+--source=projects/$DEVSHELL_PROJECT_ID/instances/ecommerce-recommendations/clusters/ecommerce-recommendations-c1/backups/PersonalizedProducts_7 \
+--async \
+--destination=PersonalizedProducts_7_restored \
+--destination-instance=ecommerce-recommendations \
+--project=$DEVSHELL_PROJECT_ID
+
+sleep 100
+
+gcloud dataflow jobs run import-sessions --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=SessionHistory,sourcePattern=gs://cloud-training/OCBL377/retail-engagements-sales-00000-of-00001,mutationThrottleLatencyMs=0
+
+gcloud dataflow jobs run import-recommendations --gcs-location gs://dataflow-templates-$REGION/latest/GCS_SequenceFile_to_Cloud_Bigtable --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/temp --parameters bigtableProject=$DEVSHELL_PROJECT_ID,bigtableInstanceId=ecommerce-recommendations,bigtableTableId=PersonalizedProducts,sourcePattern=gs://cloud-training/OCBL377/retail-recommendations-00000-of-00001
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
