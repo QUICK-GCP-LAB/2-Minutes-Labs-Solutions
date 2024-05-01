68acb0e5c57a984aeae175ac7872f017efe17008
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-01 18:17:57 +0530

    Commit

---
+++ IAM Custom Roles/IAM Custom Roles.md
@@ -0,0 +1,25 @@
+# IAM Custom Roles || [GSP190](https://www.cloudskillsboost.google/focuses/1035?parent=catalog) ||
+
+## Solution [here]()
+
+### Assign Veriables
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Loading%20Data%20into%20Cloud%20SQL/gsp196.sh
+
+sudo chmod +x gsp196.sh
+
+./gsp196.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ IAM Custom Roles/gsp190.sh
@@ -0,0 +1,65 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+echo 'title: "Role Editor"
+description: "Edit access for App Versions"
+stage: "ALPHA"
+includedPermissions:
+- appengine.versions.create
+- appengine.versions.delete' > role-definition.yaml
+
+gcloud iam roles create editor --project $DEVSHELL_PROJECT_ID \
+--file role-definition.yaml
+
+gcloud iam roles create viewer --project $DEVSHELL_PROJECT_ID \
+--title "Role Viewer" --description "Custom role description." \
+--permissions compute.instances.get,compute.instances.list --stage ALPHA
+
+echo 'description: Edit access for App Versions
+etag:
+includedPermissions:
+- appengine.versions.create
+- appengine.versions.delete
+- storage.buckets.get
+- storage.buckets.list
+name: projects/'$DEVSHELL_PROJECT_ID'/roles/editor
+stage: ALPHA
+title: Role Editor' > new-role-definition.yaml
+
+gcloud iam roles update editor --project $DEVSHELL_PROJECT_ID \
+--file new-role-definition.yaml --quiet
+
+gcloud iam roles update viewer --project $DEVSHELL_PROJECT_ID \
+--add-permissions storage.buckets.get,storage.buckets.list
+
+gcloud iam roles update viewer --project $DEVSHELL_PROJECT_ID \
+--stage DISABLED
+
+gcloud iam roles delete viewer --project $DEVSHELL_PROJECT_ID
+
+gcloud iam roles undelete viewer --project $DEVSHELL_PROJECT_ID
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
