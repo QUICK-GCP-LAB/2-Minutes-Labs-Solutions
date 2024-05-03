6b9f77c536c963cbe2479c90dbab17e32dea5c71
QUICK GCP LAB <quickgcplab@gmail.com>
2024-05-03 14:42:47 +0530

    Commit

--- Develop GenAI Apps with Gemini and Streamlit Challenge Lab/gsp517-1.sh
+++ Develop GenAI Apps with Gemini and Streamlit Challenge Lab/gsp517-1.sh
@@ -42,17 +42,17 @@ mv Dockerfile.txt Dockerfile
 
 gcloud storage cp chef.py gs://$DEVSHELL_PROJECT_ID-generative-ai/
 
+export PROJECT="$DEVSHELL_PROJECT_ID"
+
 python3 -m venv gemini-streamlit
 source gemini-streamlit/bin/activate
 python3 -m  pip install -r requirements.txt
-
 
 streamlit run chef.py \
   --browser.serverAddress=localhost \
   --server.enableCORS=false \
   --server.enableXsrfProtection=false \
   --server.port 8080
-
 echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
