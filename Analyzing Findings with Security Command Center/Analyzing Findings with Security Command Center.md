d692bb3a6328163fadc59fe2b5383f765883c8be
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-05 12:18:14 +0530

    Commit

---
+++ Analyzing Findings with Security Command Center/Analyzing Findings with Security Command Center.md
@@ -0,0 +1,47 @@
+# Analyzing Findings with Security Command Center || [GSP1164](https://www.cloudskillsboost.google/focuses/71931?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Detect%20and%20Investigate%20Threats%20with%20Security%20Command%20Center/gsp1125-1.sh
+
+sudo chmod +x gsp1125-1.sh
+
+./gsp1125-1.sh
+```
+* Go to `Export to Pub/Sub` from [here](https://console.cloud.google.com/security/command-center/config/continuous-exports/pubsub)
+
+* For the continuous export name, enter in `export-findings-pubsub`.
+
+### Run again the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Detect%20and%20Investigate%20Threats%20with%20Security%20Command%20Center/gsp1125-1.sh
+
+sudo chmod +x gsp1125-1.sh
+
+./gsp1125-1.sh
+```
+
+*Now go to `Export findings to Cloud Storage` from [here](https://console.cloud.google.com/security/command-center/export)
+
+* Set the filename to `findings.jsonl`
+
+* Go to `BigQuery Studio` from [here](https://console.cloud.google.com/bigquery)
+
+* NOW FOLLOW [VIDEO'S]() INSTRUCTIONS
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Analyzing Findings with Security Command Center/gsp1164-2.sh
@@ -0,0 +1,44 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud pubsub subscriptions update export-findings-pubsub-topic-sub --ack-deadline 10
+gcloud compute instances create instance-1 --zone=$ZONE \
+--machine-type e2-micro \
+--scopes=https://www.googleapis.com/auth/cloud-platform
+gcloud alpha pubsub subscriptions pull export-findings-pubsub-topic-sub --max-messages=10
+PROJECT_ID=$(gcloud config get project)
+bq --location=$REGION --apilog=/dev/null mk --dataset \
+$PROJECT_ID:continuous_export_dataset
+gcloud services enable securitycenter.googleapis.com
+gcloud scc bqexports create scc-bq-cont-export --dataset=projects/$DEVSHELL_PROJECT_ID/datasets/continuous_export_dataset --project=$DEVSHELL_PROJECT_ID
+for i in {0..2}; do
+gcloud iam service-accounts create sccp-test-sa-$i;
+gcloud iam service-accounts keys create /tmp/sa-key-$i.json \
+--iam-account=sccp-test-sa-$i@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com;
+done
+bq query --apilog=/dev/null --use_legacy_sql=false  \
+"SELECT finding_id,event_time,finding.category FROM continuous_export_dataset.findings"
+export BUCKET_NAME=scc-export-bucket-$DEVSHELL_PROJECT_ID
+
+gcloud storage buckets create gs://$BUCKET_NAME --location=$REGION
