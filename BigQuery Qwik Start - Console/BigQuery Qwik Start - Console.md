06a86cedd4d5c4263ab32972507a65117557fa38
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-12 12:48:24 +0530

    Commit

---
+++ BigQuery Qwik Start - Console/BigQuery Qwik Start - Console.md
@@ -0,0 +1,23 @@
+# BigQuery: Qwik Start - Console || [GSP072](https://www.cloudskillsboost.google/focuses/1145?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Visualizing%20Billing%20Data%20with%20Looker%20Studio/gsp622.sh
+
+sudo chmod +x gsp622.sh
+
+./gsp622.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ BigQuery Qwik Start - Console/gsp072.sh
@@ -0,0 +1,59 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+bq query --use_legacy_sql=false \
+'
+#standardSQL
+SELECT
+ weight_pounds, state, year, gestation_weeks
+FROM
+ `bigquery-public-data.samples.natality`
+ORDER BY weight_pounds DESC LIMIT 10;
+'
+
+bq mk babynames
+
+https://labshell-service-mvrcyiow4a-uc.a.run.app
+
+gsutil cp gs://spls/gsp072/baby-names.zip .
+
+unzip baby-names.zip
+
+bq load --autodetect --source_format=CSV babynames.names_2014 gs://spls/gsp072/baby-names/yob2014.txt name:string,gender:string,count:integer
+
+
+bq query --use_legacy_sql=false \
+'
+#standardSQL
+SELECT
+ name, count
+FROM
+ `babynames.names_2014`
+WHERE
+ gender = "M"
+ORDER BY count DESC LIMIT 5;
+'
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
