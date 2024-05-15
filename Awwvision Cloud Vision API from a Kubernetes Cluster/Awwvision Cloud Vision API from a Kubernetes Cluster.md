0e52bad1f03154592c2e74808842eccec84005ba
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-15 18:57:30 +0530

    Commit

---
+++ Awwvision Cloud Vision API from a Kubernetes Cluster/Awwvision Cloud Vision API from a Kubernetes Cluster.md
@@ -0,0 +1,26 @@
+# Awwvision: Cloud Vision API from a Kubernetes Cluster || [GSP066](https://www.cloudskillsboost.google/focuses/1241?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Creating%20Permanent%20Tables%20and%20Access-Controlled%20Views%20in%20BigQuery/gsp410.sh
+
+sudo chmod +x gsp410.sh
+
+./gsp410.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Awwvision Cloud Vision API from a Kubernetes Cluster/gsp066.sh
@@ -0,0 +1,59 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud config set compute/zone $ZONE
+
+gcloud container clusters create awwvision \
+    --num-nodes 2 \
+    --scopes cloud-platform
+
+gcloud container clusters get-credentials awwvision
+
+sudo apt-get install -y virtualenv
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+gsutil -m cp -r gs://spls/gsp066/cloud-vision .
+
+cd cloud-vision/python/awwvision
+
+make all
+
+sleep 40
+
+kubectl get pods
+
+sleep 10
+
+kubectl get deployments -o wide
+
+sleep 10
+
+kubectl get svc awwvision-webapp
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
