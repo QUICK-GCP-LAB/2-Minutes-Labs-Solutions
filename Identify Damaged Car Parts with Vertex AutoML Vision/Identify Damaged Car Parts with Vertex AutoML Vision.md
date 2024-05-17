10a0127a012297eb8db2a71af98a5e35bcc233cd
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-17 15:10:38 +0530

    Commit

---
+++ Identify Damaged Car Parts with Vertex AutoML Vision/Identify Damaged Car Parts with Vertex AutoML Vision.md
@@ -0,0 +1,26 @@
+# Identify Damaged Car Parts with Vertex AutoML Vision || [GSP972](https://www.cloudskillsboost.google/focuses/22020?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Awwvision%20Cloud%20Vision%20API%20from%20a%20Kubernetes%20Cluster/gsp066.sh
+
+sudo chmod +x gsp066.sh
+
+./gsp066.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Identify Damaged Car Parts with Vertex AutoML Vision/gsp972-1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gsutil mb -p $PROJECT_ID \
+    -c standard    \
+    -l $REGION \
+    gs://${BUCKET}
+
+gsutil -m cp -r gs://car_damage_lab_images/* gs://${BUCKET}
+
+gsutil cp gs://car_damage_lab_metadata/data.csv .
+
+sed -i -e "s/car_damage_lab_images/${BUCKET}/g" ./data.csv
+
+cat ./data.csv
+
+gsutil cp ./data.csv gs://${BUCKET}
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
