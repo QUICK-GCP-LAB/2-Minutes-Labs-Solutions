ae032bea5ae2a911705dd1f229eaf78f08f4b0a4
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-19 13:44:57 +0530

    Commit

---
+++ Creating a Data Transformation Pipeline with Cloud Dataprep/Creating a Data Transformation Pipeline with Cloud Dataprep.md
@@ -0,0 +1,23 @@
+# Creating a Data Transformation Pipeline with Cloud Dataprep || [GSP430](https://www.cloudskillsboost.google/focuses/4415?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Troubleshooting%20and%20Solving%20Data%20Join%20Pitfalls/gsp412.sh
+
+sudo chmod +x gsp412.sh
+
+./gsp412.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Creating a Data Transformation Pipeline with Cloud Dataprep/gsp430.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+bq mk ecommerce
+
+bq query --use_legacy_sql=false \
+'#standardSQL
+ CREATE OR REPLACE TABLE ecommerce.all_sessions_raw_dataprep
+ OPTIONS(
+   description="Raw data from analyst team to ingest into Cloud Dataprep"
+ ) AS
+ SELECT * FROM `data-to-insights.ecommerce.all_sessions_raw`
+ WHERE date = "20170801";'
+wget https://github.com/CodingWithHardik/files/raw/master/flow_Ecommerce_Analytics_Pipeline.zip
+
+cloudshell download flow_Ecommerce_Analytics_Pipeline.zip
