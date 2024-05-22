aea88c4cdce55824562c1c477f5a31a9ed27eb91
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-22 19:20:04 +0530

    Commit

---
+++ Creating and Populating a Bigtable Instance/Creating and Populating a Bigtable Instance.md
@@ -0,0 +1,30 @@
+# Creating and Populating a Bigtable Instance || [GSP1054](https://www.cloudskillsboost.google/focuses/58495?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Predict%20Visitor%20Purchases%20with%20a%20Classification%20Model%20in%20BigQuery%20ML/gsp229.sh
+
+sudo chmod +x gsp229.sh
+
+./gsp229.sh
+```
+* Go to `Create job from template` from [here](https://console.cloud.google.com/dataflow/createjob?)
+
+* Delete `table` & `Instance` from [here](https://console.cloud.google.com/bigtable/instances/personalized-sales/tables?)
+
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Creating and Populating a Bigtable Instance/gsp1054.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud services disable dataflow.googleapis.com
+
+
+gcloud services enable dataflow.googleapis.com
+
+export REGION=${ZONE%-*}
+
+gcloud bigtable instances create personalized-sales \
+            --display-name="Personalized Sales" \
+            --cluster-config=id=personalized-sales-c1,zone=$ZONE
+
+cbt -instance personalized-sales createtable UserSessions
+
+cbt -instance personalized-sales createfamily UserSessions Interactions
+
+cbt -instance personalized-sales createfamily UserSessions Sales
+
+cbt -instance personalized-sales lookup UserSessions
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID/
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
