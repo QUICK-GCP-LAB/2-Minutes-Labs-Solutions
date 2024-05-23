24a380a978f948ea7a513cde1cff264caa89b81d
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-23 20:03:42 +0530

    Commit

---
+++ Ingesting FHIR Data with the Healthcare API/Ingesting FHIR Data with the Healthcare API.md
@@ -0,0 +1,30 @@
+# Ingesting FHIR Data with the Healthcare API || [GSP457](https://www.cloudskillsboost.google/focuses/6104?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+```
+export LOCATION=
+```
+Check [here](https://console.cloud.google.com/bigtable/instances/sandiego?) and use another `ZONE` for `sandiego-traffic-sensors-c2` 
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Monitoring%20and%20Managing%20Bigtable%20Health%20and%20Performance/gsp1056.sh
+
+sudo chmod +x gsp1056.sh
+
+./gsp1056.sh
+```
+
+* Go to `DATASET` from [here](https://console.cloud.google.com/healthcare/browser?)
+
+* Go to `BigQuery Studio!` from [here](https://console.cloud.google.com/bigquery?)
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Ingesting FHIR Data with the Healthcare API/gsp457.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export PROJECT_NUMBER=$(gcloud projects list --filter=projectId:$PROJECT_ID \
+  --format="value(projectNumber)")
+export DATASET_ID=dataset1
+export FHIR_STORE_ID=fhirstore1
+export TOPIC=fhir-topic
+export HL7_STORE_ID=hl7v2store1
+
+gcloud services enable healthcare.googleapis.com
+
+sleep 20
+
+gcloud pubsub topics create $TOPIC
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset $PROJECT_ID:$DATASET_ID
+
+bq --location=$LOCATION mk --dataset --description HCAPI-dataset-de-id $PROJECT_ID:de_id
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.dataEditor
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-healthcare.iam.gserviceaccount.com \
+--role=roles/bigquery.jobUser
+
+gcloud healthcare datasets create $DATASET_ID \
+--location=$LOCATION
+
+gcloud healthcare fhir-stores create $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores update $FHIR_STORE_ID \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --pubsub-topic=projects/$PROJECT_ID/topics/$TOPIC
+
+gcloud healthcare fhir-stores create de_id \
+  --dataset=$DATASET_ID \
+  --location=$LOCATION \
+  --version=R4
+
+gcloud healthcare fhir-stores import gcs $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--gcs-uri=gs://spls/gsp457/fhir_devdays_gcp/fhir1/* \
+--content-structure=BUNDLE_PRETTY
+
+gcloud healthcare fhir-stores export bq $FHIR_STORE_ID \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.$DATASET_ID \
+--schema-type=analytics
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+sleep 180
+
+gcloud healthcare fhir-stores export bq de_id \
+--dataset=$DATASET_ID \
+--location=$LOCATION \
+--bq-dataset=bq://$PROJECT_ID.de_id \
+--schema-type=analytics
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
