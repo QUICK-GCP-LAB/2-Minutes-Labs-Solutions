737e93db8cf6be7daf691280947d0551dc4cec23
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-23 18:55:21 +0530

    Commit

---
+++ Monitoring and Managing Bigtable Health and Performance/Monitoring and Managing Bigtable Health and Performance.md
@@ -0,0 +1,23 @@
+# Monitoring and Managing Bigtable Health and Performance || [GSP1056](https://www.cloudskillsboost.google/focuses/58495?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Creating%20and%20Populating%20a%20Bigtable%20Instance/gsp1054.sh
+
+sudo chmod +x gsp1054.sh
+
+./gsp1054.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Monitoring and Managing Bigtable Health and Performance/gsp1056.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud bigtable backups create current_conditions_30 --instance=sandiego \
+  --cluster=sandiego-traffic-sensors-c1 \
+  --table=current_conditions \
+  --retention-period=30d 
+
+
+gcloud bigtable instances tables restore \
+--source=projects/$DEVSHELL_PROJECT_ID/instances/sandiego/clusters/sandiego-traffic-sensors-c1/backups/current_conditions_30 \
+--async \
+--destination=current_conditions_30_restored \
+--destination-instance=sandiego \
+--project=$DEVSHELL_PROJECT_ID
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
