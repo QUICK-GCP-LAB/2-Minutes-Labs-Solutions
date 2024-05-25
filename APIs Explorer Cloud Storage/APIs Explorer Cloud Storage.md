96485881eb5caca1c0d1c46aeb88a019639d2592
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-25 11:44:49 +0530

    Commit

---
+++ APIs Explorer Cloud Storage/APIs Explorer Cloud Storage.md
@@ -0,0 +1,23 @@
+# APIs Explorer: Cloud Storage || [GSP421](https://www.cloudskillsboost.google/focuses/3632?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/De-identifying%20DICOM%20Data%20with%20the%20Healthcare%20API/gsp626.sh
+
+sudo chmod +x gsp626.sh
+
+./gsp626.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ APIs Explorer Cloud Storage/gsp421.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID-2
+
+curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image1.png
+
+curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image2.png
+
+curl -LO raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/APIs%20Explorer%20Cloud%20Storage/demo-image1-copy.png
+
+gsutil cp demo-image1.png gs://$DEVSHELL_PROJECT_ID/demo-image1.png
+
+gsutil cp demo-image2.png gs://$DEVSHELL_PROJECT_ID/demo-image2.png
+
+gsutil cp demo-image1-copy.png gs://$DEVSHELL_PROJECT_ID-2/demo-image1-copy.png
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
