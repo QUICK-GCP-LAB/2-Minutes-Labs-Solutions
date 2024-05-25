ae64a454e30463929ccf702f0666eb72c4a75145
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-25 17:37:29 +0530

    Commit

---
+++ Setting Up Cost Control with Quota/Setting Up Cost Control with Quota.md
@@ -0,0 +1,26 @@
+# Setting Up Cost Control with Quota || [GSP651](https://www.cloudskillsboost.google/focuses/7847?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Implement%20DevOps%20Workflows%20in%20Google%20Cloud%20Challenge%20Lab/gsp330.sh
+
+sudo chmod +x gsp330.sh
+
+./gsp330.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Setting Up Cost Control with Quota/gsp651.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+bq query --use_legacy_sql=false \
+"
+SELECT
+    w1mpro_ep,
+    mjd,
+    load_id,
+    frame_id
+FROM
+    \`bigquery-public-data.wise_all_sky_data_release.mep_wise\`
+ORDER BY
+    mjd ASC
+LIMIT 500
+"
+
+gcloud alpha services quota list --service=bigquery.googleapis.com --consumer=projects/${DEVSHELL_PROJECT_ID} --filter="usage"
+
+gcloud alpha services quota update --consumer=projects/${DEVSHELL_PROJECT_ID} --service bigquery.googleapis.com --metric bigquery.googleapis.com/quota/query/usage --value 262144 --unit 1/d/{project}/{user} --force
+
+gcloud alpha services quota list --service=bigquery.googleapis.com --consumer=projects/${DEVSHELL_PROJECT_ID} --filter="usage"
+
+bq query --use_legacy_sql=false --nouse_cache \
+"
+ SELECT
+     w1mpro_ep,
+     mjd,
+     load_id,
+     frame_id
+ FROM
+     \`bigquery-public-data.wise_all_sky_data_release.mep_wise\`
+ ORDER BY
+     mjd ASC
+ LIMIT 500
+"
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
