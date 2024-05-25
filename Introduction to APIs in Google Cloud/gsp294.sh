b42da438cf53ee4e19d645568a00ffd59e4c7429
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-25 18:41:09 +0530

    Commit

---
+++ Introduction to APIs in Google Cloud/gsp294.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export OAUTH2_TOKEN=$(gcloud auth print-access-token)
+
+gcloud services enable fitness.googleapis.com
+
+echo '{
+  "name": "'"$DEVSHELL_PROJECT_ID"'-bucket",
+  "location": "us",
+  "storageClass": "multi_regional"
+}' > values.json
+
+curl -X POST --data-binary @values.json \
+    -H "Authorization: Bearer $OAUTH2_TOKEN" \
+    -H "Content-Type: application/json" \
+    "https://www.googleapis.com/storage/v1/b?project=$DEVSHELL_PROJECT_ID"
+
+curl -X POST --data-binary @/home/gcpstaging25084_student/demo-image.png \
+    -H "Authorization: Bearer $OAUTH2_TOKEN" \
+    -H "Content-Type: image/png" \
+    "https://www.googleapis.com/upload/storage/v1/b/$DEVSHELL_PROJECT_ID-bucket/o?uploadType=media&name=demo-image"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
