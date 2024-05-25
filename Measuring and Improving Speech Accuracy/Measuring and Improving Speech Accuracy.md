289b8432877179183e356a00fca409a2b902b046
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-25 12:23:35 +0530

    Commit

---
+++ Measuring and Improving Speech Accuracy/Measuring and Improving Speech Accuracy.md
@@ -0,0 +1,23 @@
+# Summarize Text using SQL and LLMs in BigQuery ML || [GSP835](https://www.cloudskillsboost.google/focuses/74646?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Summarize%20Text%20using%20SQL%20and%20LLMs%20in%20BigQuery%20ML/gsp835.sh
+
+sudo chmod +x gsp835.sh
+
+./gsp835.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Measuring and Improving Speech Accuracy/gsp758.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud services enable notebooks.googleapis.com
+
+gcloud services enable aiplatform.googleapis.com
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID
+
+cat > startup-script.sh <<EOF_END
+#!/bin/bash
+
+# Copy notebooks
+gsutil cp gs://spls/gsp758/notebook/measuring-accuracy.ipynb .
+gsutil cp gs://spls/gsp758/notebook/speech_adaptation.ipynb .
+gsutil cp gs://spls/gsp758/notebook/simple_wer_v2.py .
+
+# Run the notebooks
+jupyter nbconvert --to notebook --execute measuring-accuracy.ipynb
+jupyter nbconvert --to notebook --execute speech_adaptation.ipynb
+
+EOF_END
+
+export REGION="${ZONE%-*}"
+export NOTEBOOK_NAME="quicklab-jupyter"
+export MACHINE_TYPE="e2-standard-2"
+export STARTUP_SCRIPT_URL="gs://$DEVSHELL_PROJECT_ID/startup-script.sh"
+
+gcloud compute images list --project=deeplearning-platform-release --no-standard-images --filter="name~'tf2-ent-2-1'"
+
+gcloud notebooks instances create $NOTEBOOK_NAME \
+  --location=$ZONE \
+  --vm-image-project=deeplearning-platform-release \
+  --vm-image-family=tf-2-11-cu113-notebooks \
+  --machine-type=$MACHINE_TYPE \
+  --metadata=startup-script-url=gs://$DEVSHELL_PROJECT_ID/startup-script.sh
+
+echo -e "\e[1m\e[33mClick here: https://console.cloud.google.com/vertex-ai/workbench/user-managed?project=$DEVSHELL_PROJECT_ID\e[0m"
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
