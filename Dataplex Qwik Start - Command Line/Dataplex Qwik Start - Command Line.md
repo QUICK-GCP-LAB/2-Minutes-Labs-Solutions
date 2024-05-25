ff8afee8fae391fc617b45e42953735aff837f49
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-05-25 18:14:41 +0530

    Commit

---
+++ Dataplex Qwik Start - Command Line/Dataplex Qwik Start - Command Line.md
@@ -0,0 +1,26 @@
+# Dataplex: Qwik Start - Command Line || [GSP1144](https://www.cloudskillsboost.google/focuses/62708?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Dataplex%20Qwik%20Start%20-%20Console/gsp1143.sh
+
+sudo chmod +x gsp1143.sh
+
+./gsp1143.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Dataplex Qwik Start - Command Line/gsp1143.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud services enable dataplex.googleapis.com
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set compute/region $REGION
+
+gcloud dataplex lakes create ecommerce \
+   --location=$REGION \
+   --display-name="Ecommerce" \
+   --description="Ecommerce Domain"
+
+gcloud dataplex zones create orders-curated-zone \
+    --location=$REGION \
+    --lake=ecommerce \
+    --display-name="Orders Curated Zone" \
+    --resource-location-type=SINGLE_REGION \
+    --type=CURATED \
+    --discovery-enabled \
+    --discovery-schedule="0 * * * *"
+    
+bq mk --location=$REGION --dataset orders 
+
+gcloud dataplex assets create orders-curated-dataset \
+--location=$REGION \
+--lake=ecommerce \
+--zone=orders-curated-zone \
+--display-name="Orders Curated Dataset" \
+--resource-type=BIGQUERY_DATASET \
+--resource-name=projects/$PROJECT_ID/datasets/orders \
+--discovery-enabled 
+
+gcloud dataplex assets delete orders-curated-dataset --location=$REGION --zone=orders-curated-zone --lake=ecommerce 
+
+gcloud dataplex zones delete orders-curated-zone --location=$REGION --lake=ecommerce
+
+gcloud dataplex lakes delete ecommerce --location=$REGION
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
