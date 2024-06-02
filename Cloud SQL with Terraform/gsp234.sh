fd89ed41ae1099a5b14d95e38415448e34b740ab
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-02 18:35:43 +0530

    Commit

---
+++ Cloud SQL with Terraform/gsp234.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+mkdir sql-with-terraform
+
+cd sql-with-terraform
+
+gsutil cp -r gs://spls/gsp234/gsp234.zip .
+
+unzip gsp234.zip
+
+cat main.tf
+
+terraform init
+
+terraform plan -out=tfplan
+
+terraform apply tfplan
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
