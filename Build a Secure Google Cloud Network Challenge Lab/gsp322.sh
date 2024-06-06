884212a455ace58def7775fbe90617c9d603c68a
MD SOHRAB <61595664+Azso-Coder-Pro@users.noreply.github.com>
2024-06-06 18:50:56 +0530

    Commit

--- Build a Secure Google Cloud Network Challenge Lab/gsp322.sh
+++ Build a Secure Google Cloud Network Challenge Lab/gsp322.sh
@@ -26,6 +26,10 @@ RESET=`tput sgr0`
 echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 gcloud compute firewall-rules delete open-access --quiet
+
+gcloud compute instances start bastion \
+    --project=$DEVSHELL_PROJECT_ID \
+    --zone=$ZONE
  
 gcloud compute firewall-rules create ssh-ingress --allow=tcp:22 --source-ranges 35.235.240.0/20 --target-tags $IAP_NET_TAG --network acme-vpc
  
