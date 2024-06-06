66a02cf8a9dae0f4d05cbcbb1dc97f939540d8a9
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-06 18:34:29 +0530

    Commit

---
+++ Build a Secure Google Cloud Network Challenge Lab/Build a Secure Google Cloud Network Challenge Lab.md
@@ -0,0 +1,28 @@
+# Build a Secure Google Cloud Network: Challenge Lab || [GSP322](https://www.cloudskillsboost.google/focuses/12068?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+```
+export IAP_NET_TAG=
+export INT_NET_TAG=
+export HTTP_NET_TAG=
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/VPC%20Networking%20Cloud%20HA-VPN/gsp619.sh
+
+sudo chmod +x gsp619.sh
+
+./gsp619.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Build a Secure Google Cloud Network Challenge Lab/gsp322.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gcloud compute firewall-rules delete open-access
+ 
+gcloud compute firewall-rules create ssh-ingress --allow=tcp:22 --source-ranges 35.235.240.0/20 --target-tags $IAP_NET_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags bastion --tags=$IAP_NET_TAG --zone=$ZONE
+ 
+gcloud compute firewall-rules create http-ingress --allow=tcp:80 --source-ranges 0.0.0.0/0 --target-tags $HTTP_NET_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$HTTP_NET_TAG --zone=$ZONE
+ 
+gcloud compute firewall-rules create internal-ssh-ingress --allow=tcp:22 --source-ranges 192.168.10.0/24 --target-tags $INT_NET_TAG --network acme-vpc
+ 
+gcloud compute instances add-tags juice-shop --tags=$INT_NET_TAG --zone=$ZONE
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
