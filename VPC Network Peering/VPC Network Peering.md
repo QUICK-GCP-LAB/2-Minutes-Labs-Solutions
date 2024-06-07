4f82f4e7b268b23e6f852ffcb24711e14cfe7362
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-07 10:50:06 +0530

    Commit

---
+++ VPC Network Peering/VPC Network Peering.md
@@ -0,0 +1,30 @@
+# VPC Network Peering || [GSP193](https://www.cloudskillsboost.google/focuses/964?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export 2ND_PROJECT_ID=
+
+export ZONE=
+
+export ZONE_1=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Cloud%20CDN/gsp217.sh
+
+sudo chmod +x gsp217.sh
+
+./gsp217.sh
+```
+
+### Congratulations 🎉 for Completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ VPC Network Peering/gsp193.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+export 1ST_PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+export REGION_1="${ZONE%-*}"
+
+export REGION_2="${ZONE_1%-*}"
+
+gcloud config set project $1ST_PROJECT_ID
+
+gcloud compute networks create network-a --subnet-mode custom
+
+gcloud compute networks subnets create network-a-subnet --network network-a \
+    --range 10.0.0.0/16 --region $REGION_1
+
+gcloud compute instances create vm-a --zone $ZONE --network network-a --subnet network-a-subnet --machine-type e2-small
+
+gcloud compute firewall-rules create network-a-fw --network network-a --allow tcp:22,icmp
+
+# Switch to the second project
+gcloud config set project $2ND_PROJECT_ID
+
+# Create the custom network
+gcloud compute networks create network-b --subnet-mode custom
+
+# Create the subnet within this VPC
+gcloud compute networks subnets create network-b-subnet --network network-b \
+    --range 10.8.0.0/16 --region $REGION_2
+
+# Create the VM instance
+gcloud compute instances create vm-b --zone $ZONE_1 --network network-b --subnet network-b-subnet --machine-type e2-small
+
+# Enable SSH and ICMP firewall rules
+gcloud compute firewall-rules create network-b-fw --network network-b --allow tcp:22,icmp
+
+gcloud config set project $1ST_PROJECT_ID
+
+gcloud compute networks peerings create peer-ab \
+    --network=network-a \
+    --peer-project=$2ND_PROJECT_ID \
+    --peer-network=network-b 
+
+gcloud config set project $2ND_PROJECT_ID
+
+gcloud compute networks peerings create peer-ba \
+    --network=network-b \
+    --peer-project=$1ST_PROJECT_ID \
+    --peer-network=network-a
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
