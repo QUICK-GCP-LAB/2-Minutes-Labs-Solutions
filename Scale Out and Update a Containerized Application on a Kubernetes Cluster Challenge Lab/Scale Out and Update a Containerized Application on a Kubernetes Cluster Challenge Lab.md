093e5318f51d2720eef54b2cbf23aff819835202
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-09 13:35:39 +0530

    Commit

---
+++ Scale Out and Update a Containerized Application on a Kubernetes Cluster Challenge Lab/Scale Out and Update a Containerized Application on a Kubernetes Cluster Challenge Lab.md
@@ -0,0 +1,25 @@
+# Scale Out and Update a Containerized Application on a Kubernetes Cluster: Challenge Lab || [GSP305](https://www.cloudskillsboost.google/focuses/1739?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Develop%20Serverless%20Apps%20with%20Firebase%20Challenge%20Lab/gsp344.sh
+
+sudo chmod +x gsp344.sh
+
+./gsp344.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Scale Out and Update a Containerized Application on a Kubernetes Cluster Challenge Lab/gsp305.sh
@@ -0,0 +1,40 @@
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+
+gsutil cp gs://$DEVSHELL_PROJECT_ID/echo-web-v2.tar.gz .
+tar -xzvf echo-web-v2.tar.gz
+
+gcloud builds submit --tag gcr.io/$DEVSHELL_PROJECT_ID/echo-app:v2 .
+
+gcloud container clusters get-credentials echo-cluster --zone=$ZONE
+
+kubectl create deployment echo-web --image=gcr.io/qwiklabs-resources/echo-app:v2
+
+kubectl expose deployment echo-web --type=LoadBalancer --port 80 --target-port 8000
+
+kubectl scale deploy echo-web --replicas=2
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
