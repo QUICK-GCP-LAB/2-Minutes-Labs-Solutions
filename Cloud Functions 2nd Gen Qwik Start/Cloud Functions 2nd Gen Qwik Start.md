1d53ee0fd9c41d397744de5a62ac2f73a4537d5e
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 12:53:47 +0530

    Commit

---
+++ Cloud Functions 2nd Gen Qwik Start/Cloud Functions 2nd Gen Qwik Start.md
@@ -0,0 +1,46 @@
+# Cloud Functions 2nd Gen: Qwik Start || [GSP1089](https://www.cloudskillsboost.google/focuses/49757?parent=catalog) ||
+
+## Solution [here]()
+
+* Go to `Audit Logs` from [here](https://console.cloud.google.com/iam-admin/audit?)
+
+* Type or Paste in filter `Compute Engine API`
+
+* Check the box next to `Compute Engine API`
+
+* Now Check all `Admin Read` , `Data Read` & `Data Write`
+
+### Run the following Commands in CloudShell
+
+### Assign Variable
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+* Now Check The Score Upto `Task 6` then Process Next
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Cloud Functions 2nd Gen Qwik Start/gsp1089-2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export REGION="${ZONE%-*}"
+
+cd min-instances/
+
+SLOW_URL=$(gcloud functions describe slow-function --region $REGION --gen2 --format="value(serviceConfig.uri)")
+
+hey -n 10 -c 10 $SLOW_URL
+
+gcloud run services delete slow-function --region $REGION --quiet
+
+gcloud functions deploy slow-concurrent-function \
+  --gen2 \
+  --runtime go116 \
+  --entry-point HelloWorld \
+  --source . \
+  --region $REGION \
+  --trigger-http \
+  --allow-unauthenticated \
+  --min-instances 1 \
+  --max-instances 4 \
+  --quiet
+
+gcloud run deploy slow-concurrent-function \
+--image=$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/gcf-artifacts/slow--concurrent--function:version_1 \
+--concurrency=100 \
+--cpu=1 \
+--max-instances=4 \
+--region=$REGION \
+--project=$DEVSHELL_PROJECT_ID \
+ && gcloud run services update-traffic slow-concurrent-function --to-latest --region=$REGION
+
+echo "https://console.cloud.google.com/run/deploy/$REGION/slow-concurrent-function?project=$DEVSHELL_PROJECT_ID"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
