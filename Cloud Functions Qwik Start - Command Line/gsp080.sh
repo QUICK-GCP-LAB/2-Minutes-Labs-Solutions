6cab40e5be74b35f4dbe53ba9d9b2f3179fabc6e
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 13:12:04 +0530

    Commit

---
+++ Cloud Functions Qwik Start - Command Line/gsp080.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud config set compute/region $REGION
+
+mkdir gcf_hello_world
+
+cd gcf_hello_world
+
+cat > index.js <<'EOF_END'
+/**
+* Background Cloud Function to be triggered by Pub/Sub.
+* This function is exported by index.js, and executed when
+* the trigger topic receives a message.
+*
+* @param {object} data The event payload.
+* @param {object} context The event metadata.
+*/
+exports.helloWorld = (data, context) => {
+    const pubSubMessage = data;
+    const name = pubSubMessage.data
+        ? Buffer.from(pubSubMessage.data, 'base64').toString() : "Hello World";
+    
+    console.log(`My Cloud Function: ${name}`);
+    };
+EOF_END
+
+
+gsutil mb -p $DEVSHELL_PROJECT_ID gs://$DEVSHELL_PROJECT_ID
+
+gcloud services disable cloudfunctions.googleapis.com
+
+gcloud services enable cloudfunctions.googleapis.com
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+--member="serviceAccount:$DEVSHELL_PROJECT_ID@appspot.gserviceaccount.com" \
+--role="roles/artifactregistry.reader"
+
+sleep 30
+
+deploy_function() {
+  gcloud functions deploy helloWorld \
+  --stage-bucket gs://$DEVSHELL_PROJECT_ID \
+  --trigger-topic hello_world \
+  --runtime nodejs20 \
+    --quiet
+}
+
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $HTTP_FUNCTION --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Run service to be created..."
+    sleep 10
+  fi
+done
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
