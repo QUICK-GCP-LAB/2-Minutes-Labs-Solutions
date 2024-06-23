33a90015e7fe726b50264d0804ee71b4e15e86a2
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 14:13:25 +0530

    Commit

---
+++ Data Catalog Qwik Start/gsp729.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable datacatalog.googleapis.com
+
+bq mk demo_dataset
+
+bq cp bigquery-public-data:new_york_taxi_trips.tlc_yellow_trips_2018 $(gcloud config 
+get project):demo_dataset.trips
+
+gcloud data-catalog tag-templates create demo_tag_template \
+    --location=$LOCATION \
+    --display-name="Demo Tag Template" \
+    --field=id=source_of_data_asset,display-name="Source of data asset",type=string,required=TRUE \
+    --field=id=number_of_rows_in_data_asset,display-name="Number of rows in data asset",type=double \
+    --field=id=has_pii,display-name="Has PII",type=bool \
+    --field=id=pii_type,display-name="PII type",type='enum(Email|Social Security Number|None)'
+
+ENTRY_NAME=$(gcloud data-catalog entries lookup '//bigquery.googleapis.com/projects/'$DEVSHELL_PROJECT_ID'/datasets/demo_dataset/tables/trips' --format="value(name)")
+
+cat > tag_file.json << EOF
+  {
+    "source_of_data_asset": "tlc_yellow_trips_2018",
+    "pii_type": "None"
+  }
+EOF
+
+gcloud data-catalog tags create --entry=${ENTRY_NAME} \
+    --tag-template=demo_tag_template --tag-template-location=$LOCATION --tag-file=tag_file.json
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
