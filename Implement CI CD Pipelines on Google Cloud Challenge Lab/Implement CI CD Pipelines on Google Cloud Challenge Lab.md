76df8dd994ed052948fea7efb0d6f6285ccb3cf8
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 16:40:26 +0530

    Commit

---
+++ Implement CI CD Pipelines on Google Cloud Challenge Lab/Implement CI CD Pipelines on Google Cloud Challenge Lab.md
@@ -0,0 +1,26 @@
+# Implement CI/CD Pipelines on Google Cloud: Challenge Lab || [GSP393](https://www.cloudskillsboost.google/focuses/52826?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Implement CI CD Pipelines on Google Cloud Challenge Lab/gsp393.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+export REGION="${ZONE%-*}"
+gcloud config set compute/region $REGION
+
+gcloud services enable \
+container.googleapis.com \
+clouddeploy.googleapis.com \
+artifactregistry.googleapis.com \
+cloudbuild.googleapis.com \
+clouddeploy.googleapis.com
+
+sleep 20
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:$(gcloud projects describe $PROJECT_ID \
+--format="value(projectNumber)")-compute@developer.gserviceaccount.com \
+--role="roles/clouddeploy.jobRunner"
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:$(gcloud projects describe $PROJECT_ID \
+--format="value(projectNumber)")-compute@developer.gserviceaccount.com \
+--role="roles/container.developer"
+
+gcloud artifacts repositories create cicd-challenge \
+--description="Image registry for tutorial web app" \
+--repository-format=docker \
+--location=$REGION
+
+gcloud container clusters create cd-staging --node-locations=$ZONE --num-nodes=1 --async
+gcloud container clusters create cd-production --node-locations=$ZONE --num-nodes=1 --async
+
+cd ~/
+git clone https://github.com/GoogleCloudPlatform/cloud-deploy-tutorials.git
+cd cloud-deploy-tutorials
+git checkout c3cae80 --quiet
+cd tutorials/base
+
+envsubst < clouddeploy-config/skaffold.yaml.template > web/skaffold.yaml
+cat web/skaffold.yaml
+
+cd web
+skaffold build --interactive=false \
+--default-repo $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/cicd-challenge \
+--file-output artifacts.json
+cd ..
+
+cp clouddeploy-config/delivery-pipeline.yaml.template clouddeploy-config/delivery-pipeline.yaml
+sed -i "s/targetId: staging/targetId: cd-staging/" clouddeploy-config/delivery-pipeline.yaml
+sed -i "s/targetId: prod/targetId: cd-production/" clouddeploy-config/delivery-pipeline.yaml
+sed -i "/targetId: test/d" clouddeploy-config/delivery-pipeline.yaml
+
+gcloud config set deploy/region $REGION
+cp clouddeploy-config/delivery-pipeline.yaml.template clouddeploy-config/delivery-pipeline.yaml
+sed -i "s/targetId: staging/targetId: cd-staging/" clouddeploy-config/delivery-pipeline.yaml
+sed -i "s/targetId: prod/targetId: cd-production/" clouddeploy-config/delivery-pipeline.yaml
+sed -i "/targetId: test/d" clouddeploy-config/delivery-pipeline.yaml
+gcloud beta deploy apply --file=clouddeploy-config/delivery-pipeline.yaml
+
+gcloud beta deploy delivery-pipelines describe web-app
+
+#!/bin/bash
+
+CLUSTERS=("cd-production" "cd-staging")
+
+for cluster in "${CLUSTERS[@]}"; do
+  status=$(gcloud container clusters describe "$cluster" --format="value(status)")
+  
+  while [ "$status" != "RUNNING" ]; do
+    echo "Waiting for $cluster to be RUNNING..."
+
+    sleep 10  # Adjust the sleep duration as needed
+    status=$(gcloud container clusters describe "$cluster" --format="value(status)")
+  done
+  
+  echo "$cluster is now RUNNING. Proceeding with the next command."
+  
+done
+
+
+CONTEXTS=("cd-staging" "cd-production" )
+for CONTEXT in ${CONTEXTS[@]}
+do
+    gcloud container clusters get-credentials ${CONTEXT} --region ${REGION}
+    kubectl config rename-context gke_${PROJECT_ID}_${REGION}_${CONTEXT} ${CONTEXT}
+done
+
+
+for CONTEXT in ${CONTEXTS[@]}
+do
+    kubectl --context ${CONTEXT} apply -f kubernetes-config/web-app-namespace.yaml
+done
+
+envsubst < clouddeploy-config/target-staging.yaml.template > clouddeploy-config/target-cd-staging.yaml
+envsubst < clouddeploy-config/target-prod.yaml.template > clouddeploy-config/target-cd-production.yaml
+sed -i "s/staging/cd-staging/" clouddeploy-config/target-cd-staging.yaml
+sed -i "s/prod/cd-production/" clouddeploy-config/target-cd-production.yaml
+
+
+for CONTEXT in ${CONTEXTS[@]}
+do
+    envsubst < clouddeploy-config/target-$CONTEXT.yaml.template > clouddeploy-config/target-$CONTEXT.yaml
+    gcloud beta deploy apply --file clouddeploy-config/target-$CONTEXT.yaml
+done
+
+gcloud beta deploy releases create web-app-001 \
+--delivery-pipeline web-app \
+--build-artifacts web/artifacts.json \
+--source web/
+
+gcloud beta deploy rollouts list \
+--delivery-pipeline web-app \
+--release web-app-001
+
+# Wait for the rollout to complete
+while true; do
+  # Check the rollout status
+  status=$(gcloud beta deploy rollouts list --delivery-pipeline web-app --release web-app-001 --format="value(state)" | head -n 1)
+  
+  # If the status is SUCCESS, break out of the loop
+  if [ "$status" == "SUCCEEDED" ]; then
+    break
+  fi
+  
+  # Wait for a short duration before checking agai
+  echo "it's creating now, please wait..."
+
+  sleep 10
+done
+
+
+gcloud beta deploy releases promote \
+--delivery-pipeline web-app \
+--release web-app-001 \
+--quiet
+
+###
+
+
+# Wait for the rollout to complete
+while true; do
+  # Check the rollout status
+  status=$(gcloud beta deploy rollouts list --delivery-pipeline web-app --release web-app-001 --format="value(state)" | head -n 1)
+  
+  # If the status is SUCCESS, break out of the loop
+  if [ "$status" == "PENDING_APPROVAL" ]; then
+    break
+  fi
+  
+  # Wait for a short duration before checking again
+  echo "it's creating now, please wait..."
+  sleep 10
+done
+
+
+gcloud beta deploy rollouts approve web-app-001-to-cd-production-0001 \
+--delivery-pipeline web-app \
+--release web-app-001 \
+--quiet
+
+
+# Wait for the rollout to complete
+while true; do
+  # Check the rollout status
+  status=$(gcloud beta deploy rollouts list --delivery-pipeline web-app --release web-app-001 --format="value(state)" | head -n 1)
+  
+  # If the status is SUCCESS, break out of the loop
+  if [ "$status" == "SUCCEEDED" ]; then
+    break
+  fi
+  
+  # Wait for a short duration before checking again
+  echo "it's creating now, please wait..."
+  sleep 10
+done
+
+gcloud services enable cloudbuild.googleapis.com
+
+cd ~/
+git clone https://github.com/GoogleCloudPlatform/cloud-deploy-tutorials.git
+cd cloud-deploy-tutorials
+git checkout c3cae80 --quiet
+cd tutorials/base
+
+envsubst < clouddeploy-config/skaffold.yaml.template > web/skaffold.yaml
+cat web/skaffold.yaml
+
+cd web
+skaffold build --interactive=false \
+--default-repo $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/cicd-challenge \
+--file-output artifacts.json
+cd ..
+
+gcloud beta deploy releases create web-app-002 \
+--delivery-pipeline web-app \
+--build-artifacts web/artifacts.json \
+--source web/
+
+
+# Wait for the rollout to complete
+while true; do
+  # Check the rollout status
+  status=$(gcloud beta deploy rollouts list --delivery-pipeline web-app --release web-app-002 --format="value(state)" | head -n 1)
+  
+  # If the status is SUCCESS, break out of the loop
+  if [ "$status" == "SUCCEEDED" ]; then
+    break
+  fi
+  
+  # Wait for a short duration before checking agai
+  echo "it's creating now, please wait..."
+  sleep 10
+done
+
+gcloud deploy targets rollback cd-staging \
+   --delivery-pipeline=web-app \
+   --quiet
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
