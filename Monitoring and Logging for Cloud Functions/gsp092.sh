8eb933c4435373da21e8ebf2efef2e42540917b5
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 17:11:14 +0530

    Commit

---
+++ Monitoring and Logging for Cloud Functions/gsp092.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
+
+mkdir ~/hello-http && cd $_
+touch index.js && touch package.json
+
+cat > index.js <<EOF
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.helloWorld = (req, res) => {
+    let message = req.query.message || req.body.message || 'Hello World!';
+    res.status(200).send(message);
+  };
+  
+EOF
+
+cat > package.json <<EOF
+{
+    "name": "sample-http",
+    "version": "0.0.1"
+  }
+  
+EOF
+
+gcloud functions deploy helloWorld \
+--trigger-http \
+--runtime nodejs20 \
+--allow-unauthenticated \
+--region $REGION \
+--max-instances 5
+
+curl -LO 'https://github.com/tsenart/vegeta/releases/download/v6.3.0/vegeta-v6.3.0-linux-386.tar.gz'
+
+tar xvzf vegeta-v6.3.0-linux-386.tar.gz
+
+gcloud logging metrics create CloudFunctionLatency-Logs \
+    --project=$DEVSHELL_PROJECT_ID \
+    --description="subscribe to quicklab" \
+    --log-filter='resource.type="cloud_function" AND resource.labels.function_name="helloWorld" AND log_name="projects/$DEVSHELL_PROJECT_ID/logs/cloudaudit.googleapis.com%2Factivity" AND resource.labels.region="$REGION"'
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
