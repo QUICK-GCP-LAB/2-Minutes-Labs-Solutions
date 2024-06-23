5eb5a0d37b181db130ac86244d309d6d45f2d631
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 12:13:53 +0530

    Commit

---
+++ App Engine 3 Ways Challenge Lab/arc122.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable appengine.googleapis.com
+
+cat > prepare_disk.sh <<'EOF_END'
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+
+cd python-docs-samples/appengine/standard_python3/hello_world
+
+EOF_END
+
+export ZONE=$(gcloud compute instances list lab-setup --format 'csv[no-heading](zone)')
+
+gcloud compute scp prepare_disk.sh lab-setup:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh lab-setup --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
+
+git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
+
+cd python-docs-samples/appengine/standard_python3/hello_world
+
+gcloud app deploy
+
+sed -i 's/Hello World!/'"$MESSAGE"'/g' main.py
+
+gcloud app deploy
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
