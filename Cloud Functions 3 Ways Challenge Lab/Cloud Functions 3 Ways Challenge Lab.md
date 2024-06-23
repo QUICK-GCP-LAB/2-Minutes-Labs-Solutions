5aa59815cf959c161f11b43eb74ad23fa107abdf
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 13:06:45 +0530

    Commit

---
+++ Cloud Functions 3 Ways Challenge Lab/Cloud Functions 3 Ways Challenge Lab.md
@@ -0,0 +1,28 @@
+# Cloud Functions: 3 Ways: Challenge Lab || [ARC104](https://www.cloudskillsboost.google/focuses/61974?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export HTTP_FUNCTION=
+export FUNCTION_NAME=
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Cloud Functions 3 Ways Challenge Lab/arc104.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
+
+sleep 30
+
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$DEVSHELL_PROJECT_ID" --format='value(project_number)')
+
+SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member serviceAccount:$SERVICE_ACCOUNT \
+  --role roles/pubsub.publisher
+
+gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID
+
+export BUCKET="gs://$DEVSHELL_PROJECT_ID"
+
+mkdir ~/$FUNCTION_NAME && cd $_
+touch index.js && touch package.json
+
+cat > index.js <<EOF
+const functions = require('@google-cloud/functions-framework');
+functions.cloudEvent('$FUNCTION_NAME', (cloudevent) => {
+  console.log('A new event in your Cloud Storage bucket has been logged!');
+  console.log(cloudevent);
+});
+EOF
+
+cat > package.json <<EOF
+{
+  "name": "nodejs-functions-gen2-codelab",
+  "version": "0.0.1",
+  "main": "index.js",
+  "dependencies": {
+    "@google-cloud/functions-framework": "^2.0.0"
+  }
+}
+EOF
+
+deploy_function() {
+  gcloud functions deploy $FUNCTION_NAME \
+  --gen2 \
+  --runtime nodejs16 \
+  --entry-point $FUNCTION_NAME \
+  --source . \
+  --region $REGION \
+  --trigger-bucket $BUCKET \
+  --trigger-location $REGION \
+  --max-instances 2 \
+  --quiet
+}
+
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $FUNCTION_NAME --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Run service to be created..."
+    sleep 10
+  fi
+done
+
+cd ..
+
+mkdir ~/HTTP_FUNCTION && cd $_
+touch index.js && touch package.json
+
+cat > index.js <<EOF
+const functions = require('@google-cloud/functions-framework');
+functions.http('$HTTP_FUNCTION', (req, res) => {
+  res.status(200).send('subscribe to quikclab');
+});
+EOF
+
+
+cat > package.json <<EOF
+{
+  "name": "nodejs-functions-gen2-codelab",
+  "version": "0.0.1",
+  "main": "index.js",
+  "dependencies": {
+    "@google-cloud/functions-framework": "^2.0.0"
+  }
+}
+EOF
+
+deploy_function() {
+  gcloud functions deploy $HTTP_FUNCTION \
+  --gen2 \
+  --runtime nodejs16 \
+  --entry-point $HTTP_FUNCTION \
+  --source . \
+  --region $REGION \
+  --trigger-http \
+  --timeout 600s \
+  --max-instances 2 \
+  --min-instances 1 \
+  --quiet
+}
+
+# Loop until the Cloud Run service is created
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Run service is created
+  if gcloud run services describe $HTTP_FUNCTION --region $REGION &> /dev/null; then
+    echo "Cloud Run service is created. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Run service to be created..."
+    sleep 10
+  fi
+done
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
