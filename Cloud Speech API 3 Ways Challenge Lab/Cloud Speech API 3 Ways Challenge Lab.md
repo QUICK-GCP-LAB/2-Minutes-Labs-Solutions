72a8a086f118fd2e8d3ca4b4f3f0d85712976da6
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 13:45:51 +0530

    Commit

---
+++ Cloud Speech API 3 Ways Challenge Lab/Cloud Speech API 3 Ways Challenge Lab.md
@@ -0,0 +1,37 @@
+# Cloud Speech API 3 Ways: Challenge Lab || [ARC132](https://www.cloudskillsboost.google/focuses/67215?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=$(gcloud compute instances list lab-vm --format 'csv[no-heading](zone)')
+gcloud compute ssh lab-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+```
+```
+API_KEY=
+task_2_file_name=""
+task_3_request_file=""
+task_3_response_file=""
+task_4_sentence=""
+task_4_file=""
+task_5_sentence=""
+task_5_file=""
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Challenge Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Cloud Speech API 3 Ways Challenge Lab/arc132.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+audio_uri="gs://cloud-samples-data/speech/corbeau_renard.flac"
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+source venv/bin/activate
+
+cat > synthesize-text.json <<EOF
+
+{
+'input':{
+   'text':'Cloud Text-to-Speech API allows developers to include
+      natural-sounding, synthetic human speech as playable audio in
+      their applications. The Text-to-Speech API converts text or
+      Speech Synthesis Markup Language (SSML) input into audio data
+      like MP3 or LINEAR16 (the encoding used in WAV files).'
+},
+'voice':{
+   'languageCode':'en-gb',
+   'name':'en-GB-Standard-A',
+   'ssmlGender':'FEMALE'
+},
+'audioConfig':{
+   'audioEncoding':'MP3'
+}
+}
+
+EOF
+
+curl -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+-H "Content-Type: application/json; charset=utf-8" \
+-d @synthesize-text.json "https://texttospeech.googleapis.com/v1/text:synthesize" \
+> $task_2_file_name
+
+cat > "$task_3_request_file" <<EOF
+{
+"config": {
+"encoding": "FLAC",
+"sampleRateHertz": 44100,
+"languageCode": "fr-FR"
+},
+"audio": {
+"uri": "$audio_uri"
+}
+}
+EOF
+
+curl -s -X POST -H "Content-Type: application/json" \
+--data-binary @"$task_3_request_file" \
+"https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" \
+-o "$task_3_response_file"
+
+response=$(curl -s -X POST \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json; charset=utf-8" \
+-d "{\"q\": \"$task_4_sentence\"}" \
+"https://translation.googleapis.com/language/translate/v2?key=${API_KEY}&source=ja&target=en")
+echo "$response" > "$task_4_file"
+
+curl -s -X POST \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json; charset=utf-8" \
+-d "{\"q\": [\"$task_5_sentence\"]}" \
+"https://translation.googleapis.com/language/translate/v2/detect?key=${API_KEY}" \
+-o "$task_5_file"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
