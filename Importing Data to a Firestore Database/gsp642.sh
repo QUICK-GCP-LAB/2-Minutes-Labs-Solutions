9e7c3b3bd3fc4ba9d44705444586702e7c2f72b9
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-23 16:48:12 +0530

    Commit

---
+++ Importing Data to a Firestore Database/gsp642.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+
+gcloud firestore databases create --location=nam5
+
+git clone https://github.com/rosera/pet-theory
+
+cd pet-theory/lab01
+
+npm install @google-cloud/firestore
+
+npm install @google-cloud/logging
+
+curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/importTestData.js > importTestData.js (FILE NEED TO UPLOAD )
+
+npm install faker@5.5.3
+
+curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/createTestData.js > createTestData.js (FILE NEED TO UPLOAD )
+
+node createTestData 1000
+
+node importTestData customers_1000.csv
+
+npm install csv-parse
+
+node createTestData 20000
+
+node importTestData customers_20000.csv
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
