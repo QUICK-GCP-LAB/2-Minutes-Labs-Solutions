1a4dd745abf0485bfb9166eadd2a052afe19f299
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-25 13:18:46 +0530

    Commit

---
+++ Web Security Scanner Qwik Start/gsp112.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gsutil -m cp -r gs://spls/gsp067/python-docs-samples .
+
+cd python-docs-samples/appengine/standard_python3/hello_world
+
+sed -i "s/python37/python39/g" app.yaml
+
+gcloud app create --region=$REGION
+
+yes | gcloud app deploy
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
