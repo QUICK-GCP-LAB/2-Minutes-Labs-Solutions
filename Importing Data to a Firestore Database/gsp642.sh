8ca443261d53abe0a9ed65f0df09033f0bb0a9d2
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-29 16:04:32 +0530

    Commit

--- Importing Data to a Firestore Database/gsp642.sh
+++ Importing Data to a Firestore Database/gsp642.sh
@@ -37,11 +37,11 @@ npm install @google-cloud/firestore
 
 npm install @google-cloud/logging
 
-curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/importTestData.js > importTestData.js (FILE NEED TO UPLOAD )
+curl https://raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Importing%20Data%20to%20a%20Firestore%20Database/importTestData.js > importTestData.js
 
 npm install faker@5.5.3
 
-curl https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Importing%20Data%20to%20a%20Firestore%20Database/createTestData.js > createTestData.js (FILE NEED TO UPLOAD )
+curl https://raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Importing%20Data%20to%20a%20Firestore%20Database/createTestData.js > createTestData.js
 
 node createTestData 1000
 
