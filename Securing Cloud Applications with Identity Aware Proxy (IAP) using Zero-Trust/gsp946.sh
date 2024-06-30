1f9f0f6ac885a3ac483a748d0cec81d430cb0195
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-30 18:14:27 +0530

    Commit

---
+++ Securing Cloud Applications with Identity Aware Proxy (IAP) using Zero-Trust/gsp946.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable iap.googleapis.com
+
+git clone https://github.com/googlecodelabs/user-authentication-with-iap.git
+cd user-authentication-with-iap
+
+cd 1-HelloWorld
+
+cat main.py
+
+gcloud app create --project=$(gcloud config get-value project) --region=$REGION
+
+cat > app.yaml <<'EOF_END'
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+runtime: python38
+EOF_END
+
+yes | gcloud app deploy
+
+cd ~/user-authentication-with-iap/2-HelloUser
+
+cat > app.yaml <<'EOF_END'
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+runtime: python38
+EOF_END
+
+yes | gcloud app deploy
+
+EMAIL="$(gcloud config get-value core/account)" # This uses your current user/email
+
+LINK=$(gcloud app browse)
+
+LINKU=${LINK#https://}
+
+cat > details.json << EOF
+{
+  App name: IAP Example
+  Authorized domains: $LINKU
+  Developer contact email: $EMAIL
+}
+EOF
+
+cat details.json
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
