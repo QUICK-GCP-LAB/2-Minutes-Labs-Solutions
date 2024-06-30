8ad9fbfa12403949da08b8b8026909ccf9583be5
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-30 18:32:04 +0530

    Commit

---
+++ Debugging Apps on Google Kubernetes Engine/gsp736.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud config set compute/zone $ZONE
+
+export PROJECT_ID=$(gcloud info --format='value(config.project)')
+
+gcloud container clusters get-credentials central --zone $ZONE
+
+git clone https://github.com/xiangshen-dk/microservices-demo.git
+
+cd microservices-demo
+
+kubectl apply -f release/kubernetes-manifests.yaml
+
+sleep 30
+
+gcloud logging metrics create Error_Rate_SLI \
+  --description="awesome lab" \
+  --log-filter="resource.type=\"k8s_container\" severity=ERROR labels.\"k8s-pod/app\": \"recommendationservice\"" 
+
+cat > awesome.json <<EOF_END
+{
+  "displayName": "Error Rate SLI",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "Kubernetes Container - logging/user/Error_Rate_SLI",
+      "conditionThreshold": {
+        "filter": "resource.type = \"k8s_container\" AND metric.type = \"logging.googleapis.com/user/Error_Rate_SLI\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 0.5
+      }
+    }
+  ],
+  "alertStrategy": {
+    "autoClose": "604800s"
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_END
+
+gcloud alpha monitoring policies create --policy-from-file="awesome.json"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
