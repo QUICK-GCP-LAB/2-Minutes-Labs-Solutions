cfcb31ba2bb2abb19309c59f559fb9a6f7d82d80
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-06-30 18:43:31 +0530

    Commit

---
+++ Classify Images with TensorFlow on Google Cloud Challenge Lab/Classify Images with TensorFlow on Google Cloud Challenge Lab.md
@@ -0,0 +1,32 @@
+# Classify Images with TensorFlow on Google Cloud: Challenge Lab || [GSP398](https://www.cloudskillsboost.google/focuses/53698?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export ZONE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+* Go to `User-managed Notebooks` from [here](https://console.cloud.google.com/vertex-ai/workbench/user-managed?)
+
+```
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/QuickGcpLab) & [Discussion group](https://t.me/QuickGcpLabChats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Classify Images with TensorFlow on Google Cloud Challenge Lab/gsp398.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable \
+  compute.googleapis.com \
+  monitoring.googleapis.com \
+  logging.googleapis.com \
+  notebooks.googleapis.com \
+  aiplatform.googleapis.com \
+  artifactregistry.googleapis.com \
+  container.googleapis.com
+
+export NOTEBOOK_NAME="cnn-challenge"
+export MACHINE_TYPE="e2-standard-2"
+
+gcloud notebooks instances create $NOTEBOOK_NAME \
+  --location=$ZONE \
+  --vm-image-project=deeplearning-platform-release \
+  --vm-image-family=tf-2-11-cu113-notebooks \
+  --machine-type=$MACHINE_TYPE
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
