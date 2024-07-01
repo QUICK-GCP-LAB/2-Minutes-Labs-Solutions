e95c6d9993c45b959e14fc057ab470a59be09fa8
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-07-01 19:27:33 +0530

    Commit

--- App Engine 3 Ways Challenge Lab/arc112.sh
+++ App Engine 3 Ways Challenge Lab/arc112.sh
@@ -37,6 +37,8 @@ EOF_END
 
 export ZONE=$(gcloud compute instances list lab-setup --format 'csv[no-heading](zone)')
 
+export REGION="${ZONE%-*}"
+
 gcloud compute scp prepare_disk.sh lab-setup:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 
 gcloud compute ssh lab-setup --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
@@ -45,11 +47,13 @@ git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
 
 cd python-docs-samples/appengine/standard_python3/hello_world
 
-gcloud app deploy
+gcloud app create --region=$REGION
+
+yes | gcloud app deploy
 
 sed -i 's/Hello World!/'"$MESSAGE"'/g' main.py
 
-gcloud app deploy
+yes | gcloud app deploy
 
 echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
