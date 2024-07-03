256043be9f3360dd5778dd04e08b8a944dee3e6e
QUICK GCP LAB <quickgcplab@gmail.com>
2024-07-03 16:45:04 +0530

    Commit

--- Analyzing Findings with Security Command Center/gsp1164-2.sh
+++ Analyzing Findings with Security Command Center/gsp1164-2.sh
@@ -26,13 +26,13 @@ export REGION="${ZONE%-*}"
 
 PROJECT_ID=$(gcloud config get project)
 
-gcloud storage buckets create gs://scc-export-bucket-$DEVSHELL_PROJECT_ID --location=us-east4
+gcloud storage buckets create gs://scc-export-bucket-$DEVSHELL_PROJECT_ID --location=$REGION
 
 gcloud compute instances create instance-1 --zone=$ZONE \
 --machine-type e2-micro \
 --scopes=https://www.googleapis.com/auth/cloud-platform
 
-bq --location=us-east4 --apilog=/dev/null mk --dataset \
+bq --location=$REGION --apilog=/dev/null mk --dataset \
 $PROJECT_ID:continuous_export_dataset
 
 gcloud services enable securitycenter.googleapis.com
