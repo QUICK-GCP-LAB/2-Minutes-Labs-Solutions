cee97e4079e5635fe26468a9fe03be7350c4ef32
QUICK GCP LAB <quickgcplab@gmail.com>
2024-07-07 14:39:05 +0530

    Commit

--- Interact with Terraform Modules/gsp751.sh
+++ Interact with Terraform Modules/gsp751.sh
@@ -30,9 +30,25 @@ git checkout tags/v6.0.1 -b v6.0.1
 
 gcloud config list --format 'value(core.project)'
 
-cd examples/simple_project
+cd ~/terraform-google-network/examples/simple_project
 
 cat > variables.tf <<EOF_END
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 variable "project_id" {
   description = "The project ID to host the network in"
   default     = "$DEVSHELL_PROJECT_ID"
@@ -44,16 +60,80 @@ variable "network_name" {
 }
 EOF_END
 
+cat > main.tf <<EOF_END
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# Whenever a new major version of the network module is released, the
+# version constraint below should be updated, e.g. to ~> 4.0.
+#
+# If that new version includes provider updates, validation of this
+# example may fail until that is done.
+
+# [START vpc_custom_create]
+module "test-vpc-module" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 6.0"
+  project_id   = var.project_id # Replace this with your project ID in quotes
+  network_name = var.network_name
+  mtu          = 1460
+
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "$REGION"
+    },
+    {
+      subnet_name           = "subnet-02"
+      subnet_ip             = "10.10.20.0/24"
+      subnet_region         = "$REGION"
+      subnet_private_access = "true"
+      subnet_flow_logs      = "true"
+    },
+    {
+      subnet_name               = "subnet-03"
+      subnet_ip                 = "10.10.30.0/24"
+      subnet_region             = "$REGION"
+      subnet_flow_logs          = "true"
+      subnet_flow_logs_interval = "INTERVAL_10_MIN"
+      subnet_flow_logs_sampling = 0.7
+      subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+      subnet_flow_logs_filter   = "false"
+    }
+  ]
+}
+# [END vpc_custom_create]
+EOF_END
+
+cat > outputs.tf <<EOF_END
+output "bucket-name" {
+  description = "Bucket names."
+  value       = "module.gcs-static-website-bucket.bucket"
+}
+EOF_END
+
 terraform init
 
 terraform apply --auto-approve
 
 terraform destroy --auto-approve
 
-rm -rd terraform-google-network -f
-
-#task 2
 cd ~
+rm -rd terraform-google-network -f
 
 touch main.tf
 
