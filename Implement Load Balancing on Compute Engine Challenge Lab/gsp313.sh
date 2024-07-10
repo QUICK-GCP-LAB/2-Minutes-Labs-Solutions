a0473ae2f4bc6069b777eaae0cd21cfdafacda54
QUICK GCP LAB <quickgcplab@gmail.com>
2024-07-10 11:52:42 +0530

    Commit

--- Implement Load Balancing on Compute Engine Challenge Lab/gsp313.sh
+++ Implement Load Balancing on Compute Engine Challenge Lab/gsp313.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Define color variables
+
 BLACK=`tput setaf 0`
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
@@ -20,31 +23,15 @@ BOLD=`tput bold`
 RESET=`tput sgr0`
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
 export REGION="${ZONE%-*}"
-
-gcloud compute networks create nucleus-vpc --subnet-mode=auto
 
 gcloud compute instances create $INSTANCE \
     --zone=$ZONE \
     --machine-type=e2-micro
 
 echo "${RED}${BOLD}Task 1. ${RESET}""${WHITE}${BOLD}Create a project jumphost instance${RESET}" "${GREEN}${BOLD}Completed${RESET}"
-
-gcloud container clusters create nucleus-backend \
-    --num-nodes=1 \
-    --zone=$ZONE
-
-gcloud container clusters get-credentials nucleus-backend \
-          --zone $ZONE
-
-kubectl create deployment hello-server \
-          --image=gcr.io/google-samples/hello-app:2.0
-
-kubectl expose deployment hello-server \
-          --type=LoadBalancer \
-          --port $PORT_NO
 
 cat << EOF > startup.sh
 #! /bin/bash
@@ -53,64 +40,56 @@ apt-get install -y nginx
 service nginx start
 sed -i -- 's/nginx/Google Cloud Platform - '"\$HOSTNAME"'/' /var/www/html/index.nginx-debian.html
 EOF
-
+ 
 gcloud compute instance-templates create web-server-template \
---metadata-from-file startup-script=startup.sh \
---network nucleus-vpc \
---machine-type g1-small \
---region $ZONE
-
-gcloud compute target-pools create nginx-pool --region $REGION
-
+        --metadata-from-file startup-script=startup.sh \
+        --machine-type g1-small \
+        --region $REGION
+ 
 gcloud compute instance-groups managed create web-server-group \
---base-instance-name web-server \
---size 2 \
---template web-server-template \
---region $REGION
-
+        --base-instance-name web-server \
+        --size 2 \
+        --template web-server-template \
+        --region $REGION
+ 
 gcloud compute firewall-rules create $FIREWALL \
---allow tcp:80 \
---network nucleus-vpc
-
+        --allow tcp:80 \
+        --network default
+ 
 gcloud compute http-health-checks create http-basic-check
-
+ 
 gcloud compute instance-groups managed \
-       set-named-ports web-server-group \
-       --named-ports http:80 \
-       --region $REGION
-
+        set-named-ports web-server-group \
+        --named-ports http:80 \
+        --region $REGION
+ 
 gcloud compute backend-services create web-server-backend \
-       --protocol HTTP \
-       --http-health-checks http-basic-check \
-       --global
-
+        --protocol HTTP \
+        --http-health-checks http-basic-check \
+        --global
+ 
 gcloud compute backend-services add-backend web-server-backend \
-       --instance-group web-server-group \
-       --instance-group-region $REGION \
-       --global
-
+        --instance-group web-server-group \
+        --instance-group-region $REGION \
+        --global
+ 
 gcloud compute url-maps create web-server-map \
-       --default-service web-server-backend
-
+        --default-service web-server-backend
+ 
 gcloud compute target-http-proxies create http-lb-proxy \
-       --url-map web-server-map
-
+        --url-map web-server-map
+ 
 gcloud compute forwarding-rules create http-content-rule \
-     --global \
-     --target-http-proxy http-lb-proxy \
-     --ports 80
-
-gcloud compute forwarding-rules create $FIREWALL \
-     --global \
-     --target-http-proxy http-lb-proxy \
-     --ports 80
-
+      --global \
+      --target-http-proxy http-lb-proxy \
+      --ports 80
+ 
 gcloud compute forwarding-rules list
 
 echo "${RED}${BOLD}Task 2. ${RESET}""${WHITE}${BOLD}Set up an HTTP load balancer${RESET}" "${GREEN}${BOLD}Completed${RESET}"
 
 echo "${YELLOW}${BOLD}Note:${RESET}""${CYAN}${BOLD}You may need to wait for ${RESET}""${RED}${BOLD}5 to 7 minutes${RESET}""${CYAN}${BOLD} to get the score for this task.${RESET}"
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
