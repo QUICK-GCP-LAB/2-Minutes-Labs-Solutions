a92cacb2e396a8a65ab8c7668513683f2c448fd9
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-07-24 19:12:17 +0530

    Commit

--- Classify Text into Categories with the Natural Language API/gsp063.sh
+++ Classify Text into Categories with the Natural Language API/gsp063.sh
@@ -1,4 +1,29 @@
+#!/bin/bash
+# Define color variables
 
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
 gcloud services enable language.googleapis.com
 
@@ -38,6 +63,6 @@ bq --location=US mk --dataset $DEVSHELL_PROJECT_ID:news_classification_dataset
 
 bq mk --table $DEVSHELL_PROJECT_ID:news_classification_dataset.article_data article_text:STRING,category:STRING,confidence:FLOAT
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
