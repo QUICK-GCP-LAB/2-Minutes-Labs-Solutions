8ce57bf9ce88b8195f845f89c3734c7ccd7100e4
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-06 16:56:56 +0530

    Commit

---
+++ Arcade Hero Enter the Cloud Function Storage/arc236.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+PROJECT_ID=$(gcloud config get-value project)
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+
+git clone https://github.com/GoogleCloudPlatform/golang-samples.git
+cd golang-samples/functions/functionsv2/hellostorage/
+
+gcloud functions deploy cf-demo \
+  --runtime=go121 \
+  --region="$REGION" \
+  --source=. \
+  --entry-point=HelloStorage \
+  --trigger-bucket="$DEVSHELL_PROJECT_ID-bucket"
+
+# Variables
+SERVICE_NAME="cf-demo"
+
+# Loop until the Cloud Function is deployed
+while true; do
+  # Run the deployment command
+  deploy_function
+
+  # Check if Cloud Function is deployed
+  if gcloud functions describe "$SERVICE_NAME" --region "$REGION" &> /dev/null; then
+    echo "Cloud Function is deployed. Exiting the loop."
+    break
+  else
+    echo "Waiting for Cloud Function to be deployed..."
+    sleep 10
+  fi
+done
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
