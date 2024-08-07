15aa567f24b3eaf75ec1bbd94446aae214255a43
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-07 17:58:00 +0530

    Commit

---
+++ Build a Chat Application using the PaLM 2 API on Cloud Run/gsp1201.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gsutil cp -R gs://spls/gsp1201/chat-flask-cloudrun .
+
+cd chat-flask-cloudrun
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+export AR_REPO='chat-app-repo'
+export SERVICE_NAME='chat-flask-app'
+
+gcloud artifacts repositories create "$AR_REPO" --location="$REGION" --repository-format=Docker
+
+gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT_ID/$AR_REPO/$SERVICE_NAME"
+
+gcloud run deploy "$SERVICE_NAME" --port=8080 --image="$REGION-docker.pkg.dev/$PROJECT_ID/$AR_REPO/$SERVICE_NAME:latest" --allow-unauthenticated --region=$REGION --platform=managed --project=$PROJECT_ID --set-env-vars=GCP_PROJECT=$PROJECT_ID,GCP_REGION=$REGION
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
