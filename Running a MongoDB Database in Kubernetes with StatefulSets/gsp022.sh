7a567fd162637d4afba5bfa33cc3b6fce0490d20
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-12 14:34:50 +0530

    Commit

---
+++ Running a MongoDB Database in Kubernetes with StatefulSets/gsp022.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud auth list
+
+gcloud config set compute/zone $ZONE 
+
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+
+gcloud container clusters create hello-world --zone=$ZONE --project=$DEVSHELL_PROJECT_ID --num-nodes=2
+
+gsutil -m cp -r gs://spls/gsp022/mongo-k8s-sidecar .
+
+cd ./mongo-k8s-sidecar/example/StatefulSet/
+
+cat googlecloud_ssd.yaml
+
+kubectl apply -f googlecloud_ssd.yaml
+
+cat > mongo-statefulset.yaml <<EOF_CP
+#       Copyright 2016, Google, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: mongo
+  labels:
+    name: mongo
+spec:
+  ports:
+  - port: 27017
+    targetPort: 27017
+  clusterIP: None
+  selector:
+    role: mongo
+---
+apiVersion: apps/v1
+kind: StatefulSet
+metadata:
+  name: mongo
+spec:
+  serviceName: "mongo"
+  replicas: 3
+  selector:
+    matchLabels:
+      role: mongo
+  template:
+    metadata:
+     labels:
+        role: mongo
+        environment: test
+    spec:
+      terminationGracePeriodSeconds: 10
+      containers:
+        - name: mongo
+          image: mongo
+          command:
+            - mongod
+            - "--replSet"
+            - rs0
+          ports:
+            - containerPort: 27017
+          volumeMounts:
+            - name: mongo-persistent-storage
+              mountPath: /data/db
+        - name: mongo-sidecar
+          image: cvallance/mongo-k8s-sidecar
+          env:
+            - name: MONGO_SIDECAR_POD_LABELS
+              value: "role=mongo,environment=test"
+  volumeClaimTemplates:
+  - metadata:
+      name: mongo-persistent-storage
+      annotations:
+        volume.beta.kubernetes.io/storage-class: "fast"
+    spec:
+      accessModes: [ "ReadWriteOnce" ]
+      resources:
+        requests:
+          storage: 50Gi
+EOF_CP
+
+kubectl apply -f mongo-statefulset.yaml
+
+kubectl get statefulset
+
+sleep 100
+
+kubectl get statefulset
+
+kubectl get pods
+
+sleep 30
+
+kubectl get pods
+
+kubectl scale --replicas=5 statefulset mongo
+
+kubectl get pods
+
+sleep 60
+
+kubectl get pods
+
+kubectl scale --replicas=3 statefulset mongo
+
+kubectl get pods
+
+sleep 30
+
+kubectl get pods
+
+kubectl get pods
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
