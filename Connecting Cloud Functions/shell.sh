2595d4416b45ae8752fd2e655f004fae5110adb8
QUICK GCP LAB <quickgcplab@gmail.com>
2024-08-14 15:31:52 +0530

    Commit

--- Connecting Cloud Functions/shell.sh
+++ Connecting Cloud Functions/shell.sh
@@ -331,6 +331,69 @@ curl -H "Authorization: bearer $(gcloud auth print-identity-token)" "${FUNCTION_
 
 gcloud pubsub topics publish $TOPIC --message='{"id": 1234, "firstName": "Lucas" ,"lastName": "Sherman", "Phone": "555-555-5555"}'
 
+TOPIC=add_redis
+
+cd ~
+mkdir ~/redis-pubsub && cd $_
+touch main.py && touch requirements.txt
+
+cat > main.py <<'EOF_CP'
+import os
+import base64
+import json
+import redis
+import functions_framework
+
+redis_host = os.environ.get('REDISHOST', 'localhost')
+redis_port = int(os.environ.get('REDISPORT', 6379))
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port)
+
+# Triggered from a message on a Pub/Sub topic.
+@functions_framework.cloud_event
+def addToRedis(cloud_event):
+    # The Pub/Sub message data is stored as a base64-encoded string in the cloud_event.data property
+    # The expected value should be a JSON string.
+    json_data_str = base64.b64decode(cloud_event.data["message"]["data"]).decode()
+    json_payload = json.loads(json_data_str)
+    response_data = ""
+    if json_payload and 'id' in json_payload:
+        id = json_payload['id']
+        data = redis_client.set(id, json_data_str)
+        response_data = redis_client.get(id)
+        print(f"Added data to Redis: {response_data}")
+    else:
+        print("Message is invalid, or missing an 'id' attribute")
+EOF_CP
+
+
+cat > requirements.txt <<EOF_CP
+functions-framework==3.2.0
+redis==4.3.4
+EOF_CP
+
+deploy_function() {
+    gcloud functions deploy python-pubsub-function \
+    --runtime=python310 \
+    --region=$REGION \
+    --source=. \
+    --entry-point=addToRedis \
+    --trigger-topic=$TOPIC \
+    --vpc-connector projects/$PROJECT_ID/locations/$REGION/connectors/test-connector \
+    --set-env-vars REDISHOST=$REDIS_IP,REDISPORT=$REDIS_PORT
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+    if deploy_function; then
+        echo "Function deployed successfully..."
+        deploy_success=true
+    else
+        echo "Retrying in 20 seconds..."
+        sleep 20
+    fi
+done
+
 echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
