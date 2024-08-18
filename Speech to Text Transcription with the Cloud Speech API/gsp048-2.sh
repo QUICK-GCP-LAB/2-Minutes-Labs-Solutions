01618d11936d4a614ae4de7d5687287cc770b171
QUICK GCP LAB <quickgcplab@gmail.com>
2024-08-18 17:49:45 +0530

    Commit

--- Speech to Text Transcription with the Cloud Speech API/gsp048-2.sh
+++ Speech to Text Transcription with the Cloud Speech API/gsp048-2.sh
@@ -44,6 +44,8 @@ EOF
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
 "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
 
+cat result.json
+
 echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
