7fae6f559c9a870b50b3fa9d0c8cc2649ff89d8b
QUICK GCP LAB <quickgcplab@gmail.com>
2024-08-18 17:55:28 +0530

    Commit

--- Speech to Text Transcription with the Cloud Speech API/gsp048-2.sh
+++ Speech to Text Transcription with the Cloud Speech API/gsp048-2.sh
@@ -25,6 +25,12 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
+cat > prepare_disk.sh <<'EOF_END'
+
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=awesome")
+
+API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
 rm -f request.json
 
 cat >> request.json <<EOF
@@ -45,6 +51,14 @@ curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json 
 "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
 
 cat result.json
+
+EOF_END
+
+export ZONE=$(gcloud compute instances list linux-instance --format 'csv[no-heading](zone)')
+
+gcloud compute scp prepare_disk.sh linux-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
+
+gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 
 echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
