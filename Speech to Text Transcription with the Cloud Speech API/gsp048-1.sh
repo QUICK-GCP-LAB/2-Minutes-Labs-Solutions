f22ad6dd187bb73519473450e1eca62ea4d18667
QUICK GCP LAB <quickgcplab@gmail.com>
2024-08-18 17:48:34 +0530

    Commit

--- Speech to Text Transcription with the Cloud Speech API/gsp048-1.sh
+++ Speech to Text Transcription with the Cloud Speech API/gsp048-1.sh
@@ -25,8 +25,6 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-cat > prepare_disk.sh <<'EOF_END'
-
 gcloud services enable apikeys.googleapis.com
 
 gcloud alpha services api-keys create --display-name="awesome" 
@@ -51,13 +49,5 @@ EOF
 
 curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
 "https://speech.googleapis.com/v1/speech:recognize?key=${API_KEY}" > result.json
-
-EOF_END
-
-export ZONE=$(gcloud compute instances list linux-instance --format 'csv[no-heading](zone)')
-
-gcloud compute scp prepare_disk.sh linux-instance:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
-
-gcloud compute ssh linux-instance --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 
 echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}Check The Score${RESET}" "${GREEN}${BOLD}Upto Task 3 then Process Next${RESET}"
