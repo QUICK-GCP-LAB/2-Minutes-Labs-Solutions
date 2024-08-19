7db88c5882fc542d74a610c0fdca47b439e4ad54
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-19 19:02:40 +0530

    Commit

---
+++ Automate Data Capture at Scale with Document AI Challenge Lab/gsp367.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+export ZONE=$(gcloud compute instances list lab-vm --format 'csv[no-heading](zone)')
+export REGION="${ZONE%-*}"
+
+export BUCKET_LOCATION=$REGION
+export PROJECT_ID=$(gcloud config get-value core/project)
+
+gcloud services enable documentai.googleapis.com      
+gcloud services enable cloudfunctions.googleapis.com  
+gcloud services enable cloudbuild.googleapis.com    
+gcloud services enable geocoding-backend.googleapis.com 
+gcloud services enable eventarc.googleapis.com
+gcloud services enable run.googleapis.com
+
+mkdir ./document-ai-challenge
+gsutil -m cp -r gs://spls/gsp367/* \
+~/document-ai-challenge/
+
+ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
+
+curl -X POST \
+  -H "Authorization: Bearer $ACCESS_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "display_name": "'"$PROCESSOR"'",
+    "type": "FORM_PARSER_PROCESSOR"
+  }' \
+  "https://documentai.googleapis.com/v1/projects/$PROJECT_ID/locations/us/processors"
+
+gsutil mb -c standard -l ${BUCKET_LOCATION} -b on \
+ gs://${PROJECT_ID}-input-invoices
+gsutil mb -c standard -l ${BUCKET_LOCATION} -b on \
+ gs://${PROJECT_ID}-output-invoices
+gsutil mb -c standard -l ${BUCKET_LOCATION} -b on \
+ gs://${PROJECT_ID}-archived-invoices
+
+bq --location="US" mk  -d \
+    --description "Form Parser Results" \
+    ${PROJECT_ID}:invoice_parser_results
+cd ~/documentai-pipeline-demo/scripts/table-schema/
+bq mk --table \
+invoice_parser_results.doc_ai_extracted_entities \
+doc_ai_extracted_entities.json
+bq mk --table \
+invoice_parser_results.geocode_details \
+geocode_details.json
+
+cd ~/document-ai-challenge/scripts 
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
+  --role="roles/artifactregistry.reader"
+
+export CLOUD_FUNCTION_LOCATION=$REGION
+
+sleep 20
+
+deploy_function() {
+gcloud functions deploy process-invoices \
+--region=${CLOUD_FUNCTION_LOCATION} \
+--entry-point=process_invoice \
+--runtime=python39 \
+--service-account=${PROJECT_ID}@appspot.gserviceaccount.com \
+--source=cloud-functions/process-invoices \
+--timeout=400 \
+--env-vars-file=cloud-functions/process-invoices/.env.yaml \
+--trigger-resource=gs://${PROJECT_ID}-input-invoices \
+--trigger-event=google.storage.object.finalize \
+--no-gen2
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully...."
+    deploy_success=true
+  else
+    echo "Deployment Retrying, please wait...."
+    sleep 30
+  fi
+done
+
+PROCESSOR_ID=$(curl -X GET \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  -H "Content-Type: application/json" \
+  "https://documentai.googleapis.com/v1/projects/$PROJECT_ID/locations/us/processors" | \
+  grep '"name":' | \
+  sed -E 's/.*"name": "projects\/[0-9]+\/locations\/us\/processors\/([^"]+)".*/\1/')
+
+export PROCESSOR_ID
+
+gcloud functions deploy process-invoices \
+  --region=${CLOUD_FUNCTION_LOCATION} \
+  --entry-point=process_invoice \
+  --runtime=python39 \
+  --service-account=${PROJECT_ID}@appspot.gserviceaccount.com \
+  --source=cloud-functions/process-invoices \
+  --timeout=400 \
+  --trigger-resource=gs://${PROJECT_ID}-input-invoices \
+  --trigger-event=google.storage.object.finalize \
+  --update-env-vars=PROCESSOR_ID=${PROCESSOR_ID},PARSER_LOCATION=us,PROJECT_ID=${PROJECT_ID}
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+gsutil -m cp -r gs://cloud-training/gsp367/* \
+~/document-ai-challenge/invoices gs://${PROJECT_ID}-input-invoices/
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
