54b5305e092631ee89e74bfce432802a9ee6287b
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-20 16:28:29 +0530

    Commit

---
+++ Monitor Environments with Google Cloud Managed Service for Prometheus Challenge Lab/gsp364.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export ZONE=us-east1-c
+
+gcloud container clusters create gmp-cluster --num-nodes=3 --zone=$ZONE
+
+gcloud container clusters get-credentials gmp-cluster --zone=$ZONE
+
+kubectl create ns gmp-test
+
+kubectl -n gmp-system apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/main/examples/self-pod-monitoring.yaml
+
+kubectl -n gmp-system apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/prometheus-engine/main/examples/example-app.yaml
+
+cat > op-config.yaml <<'EOF_END'
+
+apiVersion: monitoring.googleapis.com/v1alpha1
+collection:
+  filter:
+    matchOneOf:
+    - '{job="prom-example"}'
+    - '{__name__=~"job:.+"}'
+kind: OperatorConfig
+metadata:
+  annotations:
+    components.gke.io/layer: addon
+    kubectl.kubernetes.io/last-applied-configuration: |
+      {"apiVersion":"monitoring.googleapis.com/v1alpha1","kind":"OperatorConfig","metadata":{"annotations":{"components.gke.io/layer":"addon"},"labels":{"addonmanager.kubernetes.io/mode":"Reconcile"},"name":"config","namespace":"gmp-public"}}
+  creationTimestamp: "2022-03-14T22:34:23Z"
+  generation: 1
+  labels:
+    addonmanager.kubernetes.io/mode: Reconcile
+  name: config
+  namespace: gmp-public
+  resourceVersion: "2882"
+  uid: 4ad23359-efeb-42bb-b689-045bd704f295
+EOF_END
+
+export PROJECT=$(gcloud config get-value project)
+
+gsutil mb -p $PROJECT gs://$PROJECT
+
+gsutil cp op-config.yaml gs://$PROJECT
+
+gsutil -m acl set -R -a public-read gs://$PROJECT
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
