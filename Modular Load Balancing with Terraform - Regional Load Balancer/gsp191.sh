8d1a23ecb5363c5b122ce68c7d4f1efcb539de96
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-20 11:52:23 +0530

    Commit

---
+++ Modular Load Balancing with Terraform - Regional Load Balancer/gsp191.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb
+cd ~/terraform-google-lb/examples/basic
+
+export GOOGLE_PROJECT=$(gcloud config get-value project)
+
+sed -i 's/us-central1/'"$REGION"'/g' variables.tf
+
+export TF_VAR_project_id=$DEVSHELL_PROJECT_ID
+
+terraform init
+terraform plan
+yes | terraform apply --auto-approve
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
