ca58abe89766d6cb87627370a2a207b29720a2f2
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-08-26 18:58:59 +0530

    Commit

---
+++ HTTPS Content-Based Load Balancer with Terraform/gsp206.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud auth list
+
+git clone https://github.com/GoogleCloudPlatform/terraform-google-lb-http.git
+
+cd ~/terraform-google-lb-http/examples/multi-backend-multi-mig-bucket-https-lb
+
+rm -rf main.tf
+
+wget https://raw.githubusercontent.com/quiccklabs/Labs_solutions/master/HTTPS%20Content-Based%20Load%20Balancer%20with%20Terraform/main.tf
+
+terraform init 
+
+echo $DEVSHELL_PROJECT_ID | terraform plan 
+
+echo $DEVSHELL_PROJECT_ID | terraform apply -auto-approve
+
+
+EXTERNAL_IP=$(terraform output | grep load-balancer-ip | cut -d = -f2 | xargs echo -n)
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}"http://${EXTERNAL_IP}"${RESET}"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
