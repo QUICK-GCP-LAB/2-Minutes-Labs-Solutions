bf56aadde3bc570196aafce4e93ddfbda75f3d55
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-09-02 13:34:48 +0530

    Commit

--- Authentication, Authorization, and Identity with Vault/gsp1005-1.sh
+++ Authentication, Authorization, and Identity with Vault/gsp1005-1.sh
@@ -25,6 +25,8 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
+gcloud auth list
+
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
