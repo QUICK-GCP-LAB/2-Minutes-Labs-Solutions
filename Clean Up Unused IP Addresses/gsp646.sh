78168362ebe68971515d230911bc9456c841ef19
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-09-03 13:43:33 +0530

    Commit

---
+++ Clean Up Unused IP Addresses/gsp646.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export REGION="${ZONE%-*}"
+
+gcloud services enable cloudscheduler.googleapis.com
+
+git clone https://github.com/GoogleCloudPlatform/gcf-automated-resource-cleanup.git && cd gcf-automated-resource-cleanup/
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)' 2>/dev/null)
+WORKDIR=$(pwd)
+
+cd $WORKDIR/unused-ip
+
+export USED_IP=used-ip-address
+export UNUSED_IP=unused-ip-address
+
+gcloud compute addresses create $USED_IP --project=$PROJECT_ID --region=$REGION
+gcloud compute addresses create $UNUSED_IP --project=$PROJECT_ID --region=$REGION
+
+gcloud compute addresses list --filter="region:($REGION)"
+
+export USED_IP_ADDRESS=$(gcloud compute addresses describe $USED_IP --region=$REGION --format=json | jq -r '.address')
+
+gcloud compute instances create static-ip-instance \
+--zone=$ZONE \
+--machine-type=e2-medium \
+--subnet=default \
+--address=$USED_IP_ADDRESS
+
+gcloud compute addresses list --filter="region:($REGION)"
+
+cat $WORKDIR/unused-ip/function.js | grep "const compute" -A 31
+
+gcloud services disable cloudfunctions.googleapis.com
+
+sleep 5
+
+gcloud services enable cloudfunctions.googleapis.com
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member="serviceAccount:$PROJECT_ID@appspot.gserviceaccount.com" \
+--role="roles/artifactregistry.reader"
+
+sleep 60
+
+gcloud functions deploy unused_ip_function \
+    --runtime nodejs12 \
+    --region $REGION \
+    --trigger-http \
+    --allow-unauthenticated
+
+export FUNCTION_URL=$(gcloud functions describe unused_ip_function --region=$REGION --format=json | jq -r '.httpsTrigger.url')
+
+gcloud app create --region $REGION
+
+gcloud scheduler jobs create http unused-ip-job \
+--schedule="* 2 * * *" \
+--uri=$FUNCTION_URL \
+--location=$REGION
+
+gcloud scheduler jobs run unused-ip-job \
+--location=$REGION
+
+gcloud compute addresses list --filter="region:($REGION)"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
