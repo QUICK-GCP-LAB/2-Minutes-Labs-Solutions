fc0768c357ddf586837a3e77d413dc9d074e6a6f
QUICK GCP LAB <quickgcplab@gmail.com>
2024-09-04 16:39:28 +0530

    Commit

--- Networking 101/gsp016.sh
+++ Networking 101/gsp016.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Define color variables
+
 BLACK=`tput setaf 0`
 RED=`tput setaf 1`
 GREEN=`tput setaf 2`
@@ -20,11 +23,9 @@ BOLD=`tput bold`
 RESET=`tput sgr0`
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-export REGION_1=${ZONE_1%-*}
-export REGION_2=${ZONE_2%-*}
-export REGION_3=${ZONE_3%-*}
+gcloud config set compute/region $REGION
 
 gcloud compute networks create taw-custom-network --subnet-mode custom
 
@@ -43,14 +44,11 @@ gcloud compute networks subnets create subnet-$REGION_3 \
    --region $REGION_3 \
    --range 10.2.0.0/16
 
-gcloud compute networks subnets list \
-   --network taw-custom-network
-
 gcloud compute firewall-rules create nw101-allow-http \
 --allow tcp:80 --network taw-custom-network --source-ranges 0.0.0.0/0 \
 --target-tags http
 
-gcloud compute firewall-rules create "nw101-allow-icmp" --allow icmp --network "taw-custom-network" --target-tags rules
+gcloud compute firewall-rules create "nw101-allow-icmp" --allow icmp --network "taw-custom-network" --source-ranges 0.0.0.0/0 --target-tags rules
 
 gcloud compute firewall-rules create "nw101-allow-internal" --allow tcp:0-65535,udp:0-65535,icmp --network "taw-custom-network" --source-ranges "10.0.0.0/16","10.2.0.0/16","10.1.0.0/16"
 
@@ -58,24 +56,6 @@ gcloud compute firewall-rules create "nw101-allow-ssh" --allow tcp:22 --network 
 
 gcloud compute firewall-rules create "nw101-allow-rdp" --allow tcp:3389 --network "taw-custom-network"
 
-gcloud compute instances create us-test-01 \
---subnet subnet-$REGION_1 \
---zone $ZONE_1 \
---machine-type e2-standard-2 \
---tags ssh,http,rules
-
-gcloud compute instances create us-test-02 \
---subnet subnet-$REGION_2 \
---zone $ZONE_2 \
---machine-type e2-standard-2 \
---tags ssh,http,rules
-
-gcloud compute instances create us-test-03 \
---subnet subnet-$REGION_3 \
---zone $ZONE_3 \
---machine-type e2-standard-2 \
---tags ssh,http,rules
-
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
 #-----------------------------------------------------end----------------------------------------------------------#
