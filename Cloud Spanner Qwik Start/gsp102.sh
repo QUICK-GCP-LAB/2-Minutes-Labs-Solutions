507af1292c6af141f256240242fb6ea98b430309
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-09-04 18:50:32 +0530

    Commit

---
+++ Cloud Spanner Qwik Start/gsp102.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud spanner instances create test-instance \
+  --config=regional-$REGION \
+  --description="Test Instance" \
+  --nodes=1
+
+
+gcloud spanner databases create example-db --instance=test-instance
+
+gcloud spanner databases ddl update example-db --instance=test-instance \
+  --ddl="CREATE TABLE Singers (
+    SingerId INT64 NOT NULL,
+    FirstName STRING(1024),
+    LastName STRING(1024),
+    SingerInfo BYTES(MAX),
+    BirthDate DATE,
+    ) PRIMARY KEY(SingerId);"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
