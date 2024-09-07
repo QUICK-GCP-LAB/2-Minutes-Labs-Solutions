5bd2ee660bb2020dc5ac3b25a4f7d2e4000abec2
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-09-07 17:32:35 +0530

    Commit

---
+++ Log Analytics on Google Cloud/gsp1088.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+export REGION=$(gcloud container clusters list --format='value(LOCATION)')
+
+gcloud container clusters get-credentials day2-ops --region $REGION
+
+git clone https://github.com/GoogleCloudPlatform/microservices-demo.git
+
+cd microservices-demo
+
+kubectl apply -f release/kubernetes-manifests.yaml
+
+sleep 60
+
+export EXTERNAL_IP=$(kubectl get service frontend-external -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+echo $EXTERNAL_IP
+
+curl -o /dev/null -s -w "%{http_code}\n"  http://${EXTERNAL_IP}
+
+gcloud logging buckets update _Default \
+    --location=global \
+    --enable-analytics
+
+gcloud logging sinks create day2ops-sink \
+    logging.googleapis.com/projects/$DEVSHELL_PROJECT_ID/locations/global/buckets/day2ops-log \
+    --log-filter='resource.type="k8s_container"' \
+    --include-children \
+    --format='json'
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
