d6e6ad93ecb49c0bdc2ced206883f778eae7837c
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-09-13 17:43:45 +0530

    Commit

---
+++ Using Cloud PubSub with Cloud Run [APPRUN]/shell.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable pubsub.googleapis.com
+gcloud services enable run.googleapis.com
+
+gcloud config set compute/region $LOCATION
+
+ gcloud run deploy store-service \
+  --image gcr.io/qwiklabs-resources/gsp724-store-service \
+  --region $LOCATION \
+  --allow-unauthenticated
+
+ gcloud run deploy order-service \
+  --image gcr.io/qwiklabs-resources/gsp724-order-service \
+  --region $LOCATION \
+  --no-allow-unauthenticated
+
+gcloud pubsub topics create ORDER_PLACED
+
+ gcloud iam service-accounts create pubsub-cloud-run-invoker \
+  --display-name "Order Initiator"
+
+ gcloud iam service-accounts list --filter="Order Initiator"
+
+ gcloud run services add-iam-policy-binding order-service --region $LOCATION \
+  --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+  --role=roles/run.invoker --platform managed
+
+export PROJECT_NUMBER=$(gcloud projects list \
+  --filter="qwiklabs-gcp" \
+  --format='value(PROJECT_NUMBER)')
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+   --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com \
+   --role=roles/iam.serviceAccountTokenCreator
+
+export ORDER_SERVICE_URL=$(gcloud run services describe order-service \
+   --region $LOCATION \
+   --format="value(status.address.url)")
+
+gcloud pubsub subscriptions create order-service-sub \
+   --topic ORDER_PLACED \
+   --push-endpoint=$ORDER_SERVICE_URL \
+   --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
