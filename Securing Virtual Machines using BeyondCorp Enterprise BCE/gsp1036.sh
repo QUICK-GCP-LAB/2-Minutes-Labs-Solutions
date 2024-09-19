ff9f34a12d431f7afdb3c418c408867f3b379707
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2024-09-19 19:23:08 +0300

    Update gsp1036.sh

--- Securing Virtual Machines using BeyondCorp Enterprise BCE/gsp1036.sh
+++ Securing Virtual Machines using BeyondCorp Enterprise BCE/gsp1036.sh
@@ -18,7 +18,6 @@ BG_WHITE=`tput setab 7`
 
 BOLD=`tput bold`
 RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
 
 echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
@@ -35,13 +34,12 @@ gcloud compute instances create windows-iap \
     --zone=$ZONE \
     --machine-type=e2-medium \
     --network-interface=stack-type=IPV4_ONLY,subnet=default,no-address \
-    --create-disk=auto-delete=yes,boot=yes,device-name=windows-iap,image=projects/windows-cloud/global/images/windows-server-2016-dc-v20240313,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=windows-iap,image=projects/windows-cloud/global/images/windows-server-2016-dc-v20240313,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-standard \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
     --labels=goog-ec-src=vm_add-gcloud \
     --reservation-affinity=any
-
 
 gcloud compute instances create windows-connectivity \
     --project=$DEVSHELL_PROJECT_ID \
@@ -52,7 +50,7 @@ gcloud compute instances create windows-connectivity \
     --maintenance-policy=MIGRATE \
     --provisioning-model=STANDARD \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
-    --create-disk=auto-delete=yes,boot=yes,device-name=windows-connectivity,image=projects/qwiklabs-resources/global/images/iap-desktop-v001,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=windows-connectivity,image=projects/qwiklabs-resources/global/images/iap-desktop-v001,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-standard \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
