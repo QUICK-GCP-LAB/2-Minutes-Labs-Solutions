c3a80a51a2804d6b56945e013c6d7f0fb614c650
Sasha Yandola <78899037+SashaYandola@users.noreply.github.com>
2024-09-23 16:20:47 +0300

    Update gsp1036.sh

--- Securing Virtual Machines using BeyondCorp Enterprise BCE/gsp1036.sh
+++ Securing Virtual Machines using BeyondCorp Enterprise BCE/gsp1036.sh
@@ -1,23 +1,24 @@
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
+BLACK=tput setaf 0
+RED=tput setaf 1
+GREEN=tput setaf 2
+YELLOW=tput setaf 3
+BLUE=tput setaf 4
+MAGENTA=tput setaf 5
+CYAN=tput setaf 6
+WHITE=tput setaf 7
 
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
+BG_BLACK=tput setab 0
+BG_RED=tput setab 1
+BG_GREEN=tput setab 2
+BG_YELLOW=tput setab 3
+BG_BLUE=tput setab 4
+BG_MAGENTA=tput setab 5
+BG_CYAN=tput setab 6
+BG_WHITE=tput setab 7
 
-BOLD=`tput bold`
-RESET=`tput sgr0`
+BOLD=tput bold
+RESET=tput sgr0
+#----------------------------------------------------start--------------------------------------------------#
 
 echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
@@ -34,12 +35,13 @@ gcloud compute instances create windows-iap \
     --zone=$ZONE \
     --machine-type=e2-medium \
     --network-interface=stack-type=IPV4_ONLY,subnet=default,no-address \
-    --create-disk=auto-delete=yes,boot=yes,device-name=windows-iap,image=projects/windows-cloud/global/images/windows-server-2016-dc-v20240313,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-standard \
+    --create-disk=auto-delete=yes,boot=yes,device-name=windows-iap,image=projects/windows-cloud/global/images/windows-server-2016-dc-v20240313,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
     --labels=goog-ec-src=vm_add-gcloud \
     --reservation-affinity=any
+
 
 gcloud compute instances create windows-connectivity \
     --project=$DEVSHELL_PROJECT_ID \
@@ -50,7 +52,7 @@ gcloud compute instances create windows-connectivity \
     --maintenance-policy=MIGRATE \
     --provisioning-model=STANDARD \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
-    --create-disk=auto-delete=yes,boot=yes,device-name=windows-connectivity,image=projects/qwiklabs-resources/global/images/iap-desktop-v001,mode=rw,size=20,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-standard \
+    --create-disk=auto-delete=yes,boot=yes,device-name=windows-connectivity,image=projects/qwiklabs-resources/global/images/iap-desktop-v001,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
