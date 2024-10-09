652d3368026a99b05556ffd87ca3e54d2b3634fe
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-09 17:01:54 +0530

    Commit

---
+++ Implement continuous delivery with Gemini/Implement continuous delivery with Gemini.md
@@ -0,0 +1,26 @@
+# [Implement continuous delivery with Gemini](https://www.cloudskillsboost.google/course_templates/882/labs/476337)
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export REGION=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/quickgcplab) & [Discussion group](https://t.me/quickgcplabchats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Implement continuous delivery with Gemini/shell.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+PROJECT_ID=$(gcloud config get-value project)
+echo "PROJECT_ID=${PROJECT_ID}"
+echo "REGION=${REGION}"
+
+USER=$(gcloud config get-value account 2> /dev/null)
+echo "USER=${USER}"
+
+gcloud services enable cloudaicompanion.googleapis.com --project ${PROJECT_ID}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/cloudaicompanion.user
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/serviceusage.serviceUsageViewer
+
+gcloud services enable container.googleapis.com --project ${PROJECT_ID}
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/container.admin
+
+gcloud container clusters create test \
+    --project=$DEVSHELL_PROJECT_ID \
+    --zone=$REGION \
+    --num-nodes=3 \
+    --machine-type=e2-standard-4
+
+git clone --depth=1 https://github.com/GoogleCloudPlatform/microservices-demo
+
+cd ~/microservices-demo
+kubectl apply -f ./release/kubernetes-manifests.yaml
+
+sleep 120
+
+kubectl get deployments
+
+echo "http://$(kubectl get service frontend-external -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')"
+
+gcloud builds worker-pools create pool-test \
+  --project=$DEVSHELL_PROJECT_ID \
+  --region=$REGION \
+  --no-public-egress
+
+gcloud artifacts repositories create my-repo \
+  --repository-format=docker \
+  --location=$REGION \
+  --description="My private Docker repository"
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
