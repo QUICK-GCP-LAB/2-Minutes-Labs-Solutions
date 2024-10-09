dd03908397a2e0ff7c2fc16de7c7ae0b52c0d967
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-09 18:07:32 +0530

    Commit

---
+++ Ingesting New Datasets into BigQuery/gsp411.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+curl -LO 
+
+curl -LO 
+
+bq mk ecommerce
+
+gsutil mb gs://$DEVSHELL_PROJECT_ID/
+
+gsutil cp products.csv gs://$DEVSHELL_PROJECT_ID/
+
+gsutil cp gsp411.csv gs://$DEVSHELL_PROJECT_ID/
+
+
+bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 ecommerce.products gs://$DEVSHELL_PROJECT_ID/products.csv
+
+bq --location=US load --source_format=CSV --autodetect --skip_leading_rows=1 ecommerce.products gs://data-insights-course/exports/products.csv
+
+
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+  *,
+  SAFE_DIVIDE(orderedQuantity,stockLevel) AS ratio
+FROM
+  ecommerce.products
+WHERE
+# include products that have been ordered and
+# are 80% through their inventory
+orderedQuantity > 0
+AND SAFE_DIVIDE(orderedQuantity,stockLevel) >= .8
+ORDER BY
+  restockingLeadTime DESC
+"
+
+cat > external_table_definition.json <<EOF
+{
+  "sourceFormat": "GOOGLE_SHEETS",
+  "sourceUris": ["https://docs.google.com/spreadsheets/d/1Pyr2ifVgC82eCDNxBKgEXc33fkzMTPa2/edit?usp=sharing"],
+  "schema": {
+    "fields": [
+      {"name": "column1", "type": "STRING"},
+      {"name": "column2", "type": "INTEGER"},
+      {"name": "column3", "type": "FLOAT"}
+    ]
+  }
+}
+EOF
+
+bq mk --external_table_definition=external_table_definition.json ecommerce.products_comments
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
