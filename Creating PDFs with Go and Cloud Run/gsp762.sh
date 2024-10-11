09bee8d6534ffe8e4c4734f94adabe212e48b34f
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-11 17:29:39 +0530

    Commit

---
+++ Creating PDFs with Go and Cloud Run/gsp762.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable storage-component.googleapis.com
+gcloud services enable run.googleapis.com
+
+gcloud auth list --filter=status:ACTIVE --format="value(account)"
+
+git clone https://github.com/Deleplace/pet-theory.git
+
+cd pet-theory/lab03
+
+curl -o 
+
+go build -o server
+
+cat > Dockerfile <<EOF_END
+FROM debian:buster
+RUN apt-get update -y \
+  && apt-get install -y libreoffice \
+  && apt-get clean
+WORKDIR /usr/src/app
+COPY server .
+CMD [ "./server" ]
+EOF_END
+
+gcloud builds submit \
+  --tag gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter
+
+gcloud run deploy pdf-converter \
+  --image gcr.io/$GOOGLE_CLOUD_PROJECT/pdf-converter \
+  --platform managed \
+  --region $REGION \
+  --memory=2Gi \
+  --no-allow-unauthenticated \
+  --set-env-vars PDF_BUCKET=$GOOGLE_CLOUD_PROJECT-processed \
+  --max-instances=3
+
+gsutil notification create -t new-doc -f json -e OBJECT_FINALIZE gs://$GOOGLE_CLOUD_PROJECT-upload
+
+gcloud iam service-accounts create pubsub-cloud-run-invoker --display-name "PubSub Cloud Run Invoker"
+
+gcloud run services add-iam-policy-binding pdf-converter \
+  --member=serviceAccount:pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com \
+  --role=roles/run.invoker \
+  --region $REGION \
+  --platform managed
+
+PROJECT_NUMBER=$(gcloud projects list \
+ --format="value(PROJECT_NUMBER)" \
+ --filter="$GOOGLE_CLOUD_PROJECT")
+
+gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT \
+  --member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-pubsub.iam.gserviceaccount.com \
+  --role=roles/iam.serviceAccountTokenCreator
+
+SERVICE_URL=$(gcloud run services describe pdf-converter \
+  --platform managed \
+  --region $REGION \
+  --format "value(status.url)")
+
+gcloud pubsub subscriptions create pdf-conv-sub \
+  --topic new-doc \
+  --push-endpoint=$SERVICE_URL \
+  --push-auth-service-account=pubsub-cloud-run-invoker@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
