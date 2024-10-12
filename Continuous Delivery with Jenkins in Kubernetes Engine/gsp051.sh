1f04bf13459dac71db1518565ab4410acdb4e99e
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-12 15:54:18 +0530

    Commit

---
+++ Continuous Delivery with Jenkins in Kubernetes Engine/gsp051.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud config set compute/zone $ZONE
+
+gsutil cp gs://spls/gsp051/continuous-deployment-on-kubernetes.zip .
+
+unzip continuous-deployment-on-kubernetes.zip
+
+cd continuous-deployment-on-Kubernetes
+
+gcloud container clusters create jenkins-cd \
+--num-nodes 2 \
+--machine-type e2-standard-2 \
+--scopes "https://www.googleapis.com/auth/source.read_write,cloud-platform"
+
+gcloud container clusters list
+
+gcloud container clusters get-credentials jenkins-cd
+
+kubectl cluster-info
+
+helm repo add jenkins https://charts.jenkins.io
+
+helm repo update
+
+helm install cd jenkins/jenkins -f jenkins/values.yaml --wait
+
+SLEEP 80
+
+kubectl create clusterrolebinding jenkins-deploy --clusterrole=cluster-admin --serviceaccount=default:cd-jenkins
+
+export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/component=jenkins-master" -l "app.kubernetes.io/instance=cd" -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8080:8080 >> /dev/null &
+
+kubectl get svc
+
+printf $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
+
+cd sample-app
+
+kubectl create ns production
+
+kubectl apply -f k8s/production -n production
+
+kubectl apply -f k8s/canary -n production
+
+kubectl apply -f k8s/services -n production
+
+kubectl scale deployment gceme-frontend-production -n production --replicas 4
+
+kubectl get pods -n production -l app=gceme -l role=frontend
+
+kubectl get pods -n production -l app=gceme -l role=backend
+
+kubectl get service gceme-frontend -n production
+
+export FRONTEND_SERVICE_IP=$(kubectl get -o jsonpath="{.status.loadBalancer.ingress[0].ip}" --namespace=production services gceme-frontend)
+
+curl http://$FRONTEND_SERVICE_IP/version
+
+gcloud source repos create default
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
