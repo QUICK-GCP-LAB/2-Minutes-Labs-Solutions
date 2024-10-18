2b6ac07b6c6b5348ba77782ff6773d818a3b8be9
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-18 17:48:22 +0530

    Commit

---
+++ Building a DevOps Pipeline/shell-1.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud source repos create devops-repo
+
+mkdir gcp-course
+
+cd gcp-course
+
+gcloud source repos clone devops-repo
+
+cd devops-repo
+
+cat > main.py <<'EOF_END'
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    model = {"title": "Hello DevOps Fans."}
+    return render_template('index.html', model=model)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+EOF_END
+
+mkdir templates
+
+cd templates
+
+cat > layout.html <<'EOF_END'
+<!doctype html>
+<html lang="en">
+<head>
+    <title>{{model.title}}</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div class="container">
+
+        {% block content %}{% endblock %}
+
+        <footer></footer>
+    </div>
+</body>
+</html>
+EOF_END
+
+cat > index.html <<'EOF_END'
+{% extends "layout.html" %}
+{% block content %}
+<div class="jumbotron">
+    <div class="container">
+        <h1>{{model.title}}</h1>
+    </div>
+</div>
+{% endblock %}
+EOF_END
+
+cd ~/gcp-course/devops-repo
+
+cat > requirements.txt <<'EOF_END'
+Flask>=2.0.3
+EOF_END
+
+git add --all
+
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name student
+
+git commit -a -m "Initial Commit"
+
+git push origin master
+
+cat > Dockerfile <<'EOF_END'
+FROM python:3.9
+WORKDIR /app
+COPY . .
+RUN pip install gunicorn
+RUN pip install -r requirements.txt
+ENV PORT=80
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 main:app
+EOF_END
+
+echo $DEVSHELL_PROJECT_ID
+
+gcloud artifacts repositories create devops-repo \
+    --repository-format=docker \
+    --location=$REGION
+
+gcloud auth configure-docker $REGION-docker.pkg.dev
+
+gcloud builds submit --tag $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/devops-repo/devops-image:v0.1 .
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}""https://console.cloud.google.com/compute/instancesAdd?"""${RESET}"
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
---
+++ Building a DevOps Pipeline/shell-2.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+cd ~/gcp-course/devops-repo
+git add --all
+
+git commit -am "Added Docker Support"
+
+git push origin master
+
+cat > cloudbuild.yaml <<'EOF_END'
+steps:
+  - name: 'gcr.io/cloud-builders/docker'
+    args: ['build', '-t', '$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/devops-repo/devops-image:$COMMIT_SHA', '.']
+images:
+  - '$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/devops-repo/devops-image:$COMMIT_SHA'
+options:
+  logging: CLOUD_LOGGING_ONLY
+EOF_END
+
+gcloud builds triggers create cloud-source-repositories \
+    --name="devops-trigger" \
+    --service-account="projects/$DEVSHELL_PROJECT_ID/serviceAccounts/$DEVSHELL_PROJECT_ID@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com"  \
+    --description="Awesome" \
+    --repo="devops-repo" \
+    --branch-pattern=".*" \
+    --build-config="cloudbuild.yaml"
+
+cat > main.py <<'EOF_END'
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    model = {"title": "Hello Build Trigger."}
+    return render_template('index.html', model=model)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+EOF_END
+
+cd ~/gcp-course/devops-repo
+git commit -a -m "Testing Build Trigger"
+
+git push origin master
+
+export IMAGE=$(gcloud artifacts docker images describe \
+    $REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/devops-repo/devops-image:v0.1 \
+    --format 'value(image_summary.digest)')
+
+echo "${CYAN}${BOLD}Copy this: "${RESET}""${GREEN}${BOLD}""$REGION-docker.pkg.dev/$DEVSHELL_PROJECT_ID/devops-repo/devops-image@$IMAGE"""${RESET}"
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}""https://console.cloud.google.com/compute/instancesAdd?"""${RESET}"
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
