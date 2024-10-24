da18e8082816b5d0560d6db8de649afe488ff4a1
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-10-24 14:04:42 +0530

    Commit

---
+++ Monitor and Log with Google Cloud Observability Challenge Lab/Monitor and Log with Google Cloud Observability Challenge Lab.md
@@ -0,0 +1,29 @@
+# Monitor and Log with Google Cloud Observability: Challenge Lab || [GSP338](https://www.cloudskillsboost.google/focuses/13786?parent=catalog) ||
+
+## Solution [here]()
+
+### Run the following Commands in CloudShell
+
+```
+export METRIC=
+export VALUE=
+```
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+* Type or paste **input_queue_size**
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/quickgcplab) & [Discussion group](https://t.me/quickgcplabchats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Monitor and Log with Google Cloud Observability Challenge Lab/gsp338.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+gcloud services enable monitoring.googleapis.com
+
+export ZONE=$(gcloud compute instances list video-queue-monitor --format 'csv[no-heading](zone)')
+
+export REGION="${ZONE%-*}"
+
+export INSTANCE_ID=$(gcloud compute instances describe video-queue-monitor --project="$DEVSHELL_PROJECT_ID" --zone="$ZONE" --format="get(id)")
+
+gcloud compute instances stop video-queue-monitor --zone $ZONE
+
+cat > startup-script.sh <<EOF_START
+#!/bin/bash
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+export REGION=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+## Install Golang
+sudo apt update && sudo apt -y
+sudo apt-get install wget -y
+sudo apt-get -y install git
+sudo chmod 777 /usr/local/
+sudo wget https://go.dev/dl/go1.22.8.linux-amd64.tar.gz 
+sudo tar -C /usr/local -xzf go1.22.8.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
+# Install ops agent 
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo service google-cloud-ops-agent start
+
+# Create go working directory and add go path
+mkdir /work
+mkdir /work/go
+mkdir /work/go/cache
+export GOPATH=/work/go
+export GOCACHE=/work/go/cache
+
+# Install Video queue Go source code
+cd /work/go
+mkdir video
+gsutil cp gs://spls/gsp338/video_queue/main.go /work/go/video/main.go
+
+# Get Cloud Monitoring (stackdriver) modules
+go get go.opencensus.io
+go get contrib.go.opencensus.io/exporter/stackdriver
+
+# Configure env vars for the Video Queue processing application
+export MY_PROJECT_ID=$DEVSHELL_PROJECT_ID
+export MY_GCE_INSTANCE_ID=$INSTANCE_ID
+export MY_GCE_INSTANCE_ZONE=$ZONE
+
+# Initialize and run the Go application
+cd /work
+go mod init go/video/main
+go mod tidy
+go run /work/go/video/main.go
+EOF_START
+
+gcloud compute instances add-metadata video-queue-monitor \
+  --zone $ZONE \
+  --metadata-from-file startup-script=startup-script.sh
+
+gcloud compute instances start video-queue-monitor --zone $ZONE
+
+gcloud logging metrics create $METRIC \
+    --description="Metric for high resolution video uploads" \
+    --log-filter='textPayload=("file_format=4K" OR "file_format=8K")'
+
+cat > email-channel.json <<EOF_END
+{
+  "type": "email",
+  "displayName": "quickgcplab",
+  "description": "Awesome",
+  "labels": {
+    "email_address": "$USER_EMAIL"
+  }
+}
+EOF_END
+
+gcloud beta monitoring channels create --channel-content-from-file="email-channel.json"
+
+email_channel_info=$(gcloud beta monitoring channels list)
+email_channel_id=$(echo "$email_channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
+
+cat > quickgcplab.json <<EOF_END
+{
+  "displayName": "quickgcplab",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - logging/user/large_video_upload_rate",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"logging.googleapis.com/user/large_video_upload_rate\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "300s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_RATE"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": $VALUE
+      }
+    }
+  ],
+  "alertStrategy": {
+    "notificationPrompts": [
+      "OPENED"
+    ]
+  },
+  "combiner": "OR",
+  "enabled": true,
+  "notificationChannels": [
+    "$email_channel_id"
+  ],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_END
+
+# Create the alert policy
+gcloud alpha monitoring policies create --policy-from-file=quickgcplab.json
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}""https://console.cloud.google.com/monitoring/dashboards?project=$DEVSHELL_PROJECT_ID"""${RESET}"
+
+echo "${YELLOW}${BOLD}NOW${RESET}" "${WHITE}${BOLD}FOLLOW${RESET}" "${GREEN}${BOLD}VIDEO'S INSTRUCTIONS${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
