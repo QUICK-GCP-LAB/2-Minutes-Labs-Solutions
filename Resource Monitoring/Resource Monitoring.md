6dd620865183626950880eeb1387dc0b057c956c
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-11-01 15:22:28 +0530

    Commit

---
+++ Resource Monitoring/Resource Monitoring.md
@@ -0,0 +1,35 @@
+# [Resource Monitoring ](https://www.cloudskillsboost.google/games/5632/labs/36136)
+
+## Solution [here]()
+
+* Go to **Dashboards Overview** from [here](https://console.cloud.google.com/monitoring/dashboards?)
+
+* Type **CPU utilization** in Metric field dropdown
+
+* Go to **Create group** from [here](https://console.cloud.google.com/monitoring/groups/create?)
+
+* Enter a name for the group. For example: **VM instances**
+
+* In the Criteria section, type **nginx** in the Value field below Contains.
+
+* Go to **Create Uptime Check** from [here](https://console.cloud.google.com/monitoring/uptime/create?)
+
+### Run the following Commands in CloudShell
+
+```
+curl -LO raw.githubusercontent.com/QUICK-GCP-LAB/2-Minutes-Labs-Solutions/main/Analyze%20Images%20with%20the%20Cloud%20Vision%20API%20Challenge%20Lab/arc122.sh
+
+sudo chmod +x arc122.sh
+
+./arc122.sh
+```
+
+### Congratulations 🎉 for completing the Lab !
+
+##### *You Have Successfully Demonstrated Your Skills And Determination.*
+
+#### *Well done!*
+
+#### Don't Forget to Join the [Telegram Channel](https://t.me/quickgcplab) & [Discussion group](https://t.me/quickgcplabchats)
+
+# [QUICK GCP LAB](https://www.youtube.com/@quickgcplab)
---
+++ Resource Monitoring/shell.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
+
+cat > email-channel.json <<EOF_END
+{
+  "type": "email",
+  "displayName": "quickgcplab",
+  "description": "Awesome",
+  "labels": {
+    "email_address": "$USER_EMAIL"
+  }
+}
+EOF_END
+
+gcloud beta monitoring channels create --channel-content-from-file="email-channel.json"
+
+# Get the channel ID
+email_channel_info=$(gcloud beta monitoring channels list)
+email_channel_id=$(echo "$email_channel_info" | grep -oP 'name: \K[^ ]+' | head -n 1)
+
+cat > quickgcplab.json <<EOF_END
+{
+  "displayName": "quickgcplab",
+  "userLabels": {},
+  "conditions": [
+    {
+      "displayName": "VM Instance - CPU utilization",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "60s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_MEAN"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 0.2
+      }
+    },
+    {
+      "displayName": "VM Instance - CPU utilization",
+      "conditionThreshold": {
+        "filter": "resource.type = \"gce_instance\" AND metric.type = \"compute.googleapis.com/instance/cpu/utilization\"",
+        "aggregations": [
+          {
+            "alignmentPeriod": "60s",
+            "crossSeriesReducer": "REDUCE_NONE",
+            "perSeriesAligner": "ALIGN_MEAN"
+          }
+        ],
+        "comparison": "COMPARISON_GT",
+        "duration": "0s",
+        "trigger": {
+          "count": 1
+        },
+        "thresholdValue": 0.2
+      }
+    }
+  ],
+  "alertStrategy": {
+    "notificationPrompts": [
+      "OPENED"
+    ]
+  },
+  "combiner": "AND_WITH_MATCHING_RESOURCE",
+  "enabled": true,
+  "notificationChannels": [
+    "$email_channel_id"
+  ],
+  "severity": "SEVERITY_UNSPECIFIED"
+}
+EOF_END
+
+# Create the alert policy
+gcloud alpha monitoring policies create --policy-from-file=quickgcplab.json
+
+echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
