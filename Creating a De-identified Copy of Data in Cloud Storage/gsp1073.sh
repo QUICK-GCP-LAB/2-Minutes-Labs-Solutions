ae5ef283e92ff9d013b86375381c4d23a61a4787
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-11-21 16:11:54 +0530

    Commit

--- Creating a De-identified Copy of Data in Cloud Storage/gsp1073.sh
+++ Creating a De-identified Copy of Data in Cloud Storage/gsp1073.sh
@@ -133,10 +133,249 @@ cat > job-configuration.json << EOM
       ],
       "inspectConfig": {
         "infoTypes": [
-          { "name": "EMAIL_ADDRESS" },
-          { "name": "CREDIT_CARD_NUMBER" },
-          { "name": "PHONE_NUMBER" },
-          { "name": "SSN" }
+          {
+            "name": "ADVERTISING_ID"
+          },
+          {
+            "name": "AGE"
+          },
+          {
+            "name": "ARGENTINA_DNI_NUMBER"
+          },
+          {
+            "name": "AUSTRALIA_TAX_FILE_NUMBER"
+          },
+          {
+            "name": "BELGIUM_NATIONAL_ID_CARD_NUMBER"
+          },
+          {
+            "name": "BRAZIL_CPF_NUMBER"
+          },
+          {
+            "name": "CANADA_SOCIAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "CHILE_CDI_NUMBER"
+          },
+          {
+            "name": "CHINA_RESIDENT_ID_NUMBER"
+          },
+          {
+            "name": "COLOMBIA_CDC_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_NUMBER"
+          },
+          {
+            "name": "CREDIT_CARD_TRACK_NUMBER"
+          },
+          {
+            "name": "DATE_OF_BIRTH"
+          },
+          {
+            "name": "DENMARK_CPR_NUMBER"
+          },
+          {
+            "name": "EMAIL_ADDRESS"
+          },
+          {
+            "name": "ETHNIC_GROUP"
+          },
+          {
+            "name": "FDA_CODE"
+          },
+          {
+            "name": "FINLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "FRANCE_CNI"
+          },
+          {
+            "name": "FRANCE_NIR"
+          },
+          {
+            "name": "FRANCE_TAX_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "GENDER"
+          },
+          {
+            "name": "GERMANY_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "GERMANY_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "HONG_KONG_ID_NUMBER"
+          },
+          {
+            "name": "IBAN_CODE"
+          },
+          {
+            "name": "IMEI_HARDWARE_ID"
+          },
+          {
+            "name": "INDIA_AADHAAR_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_GST_INDIVIDUAL"
+          },
+          {
+            "name": "INDIA_PAN_INDIVIDUAL"
+          },
+          {
+            "name": "INDONESIA_NIK_NUMBER"
+          },
+          {
+            "name": "IRELAND_PPSN"
+          },
+          {
+            "name": "ISRAEL_IDENTITY_CARD_NUMBER"
+          },
+          {
+            "name": "JAPAN_INDIVIDUAL_NUMBER"
+          },
+          {
+            "name": "KOREA_RRN"
+          },
+          {
+            "name": "MAC_ADDRESS"
+          },
+          {
+            "name": "MEXICO_CURP_NUMBER"
+          },
+          {
+            "name": "NETHERLANDS_BSN_NUMBER"
+          },
+          {
+            "name": "NORWAY_NI_NUMBER"
+          },
+          {
+            "name": "PARAGUAY_CIC_NUMBER"
+          },
+          {
+            "name": "PASSPORT"
+          },
+          {
+            "name": "PERSON_NAME"
+          },
+          {
+            "name": "PERU_DNI_NUMBER"
+          },
+          {
+            "name": "PHONE_NUMBER"
+          },
+          {
+            "name": "POLAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "PORTUGAL_CDC_NUMBER"
+          },
+          {
+            "name": "SCOTLAND_COMMUNITY_HEALTH_INDEX_NUMBER"
+          },
+          {
+            "name": "SINGAPORE_NATIONAL_REGISTRATION_ID_NUMBER"
+          },
+          {
+            "name": "SPAIN_CIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_DNI_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIE_NUMBER"
+          },
+          {
+            "name": "SPAIN_NIF_NUMBER"
+          },
+          {
+            "name": "SPAIN_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "STORAGE_SIGNED_URL"
+          },
+          {
+            "name": "STREET_ADDRESS"
+          },
+          {
+            "name": "SWEDEN_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "SWIFT_CODE"
+          },
+          {
+            "name": "THAILAND_NATIONAL_ID_NUMBER"
+          },
+          {
+            "name": "TURKEY_ID_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_HEALTH_SERVICE_NUMBER"
+          },
+          {
+            "name": "UK_NATIONAL_INSURANCE_NUMBER"
+          },
+          {
+            "name": "UK_TAXPAYER_REFERENCE"
+          },
+          {
+            "name": "URUGUAY_CDI_NUMBER"
+          },
+          {
+            "name": "US_BANK_ROUTING_MICR"
+          },
+          {
+            "name": "US_EMPLOYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_HEALTHCARE_NPI"
+          },
+          {
+            "name": "US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "US_SOCIAL_SECURITY_NUMBER"
+          },
+          {
+            "name": "VEHICLE_IDENTIFICATION_NUMBER"
+          },
+          {
+            "name": "VENEZUELA_CDI_NUMBER"
+          },
+          {
+            "name": "WEAK_PASSWORD_HASH"
+          },
+          {
+            "name": "AUTH_TOKEN"
+          },
+          {
+            "name": "AWS_CREDENTIALS"
+          },
+          {
+            "name": "AZURE_AUTH_TOKEN"
+          },
+          {
+            "name": "BASIC_AUTH_HEADER"
+          },
+          {
+            "name": "ENCRYPTION_KEY"
+          },
+          {
+            "name": "GCP_API_KEY"
+          },
+          {
+            "name": "GCP_CREDENTIALS"
+          },
+          {
+            "name": "JSON_WEB_TOKEN"
+          },
+          {
+            "name": "HTTP_COOKIE"
+          },
+          {
+            "name": "XSRF_TOKEN"
+          }
         ],
         "minLikelihood": "POSSIBLE"
       },
