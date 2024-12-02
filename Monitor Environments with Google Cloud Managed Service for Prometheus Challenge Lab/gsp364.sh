787e7088927167e7423eecd41c681621567cb113
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-12-02 12:33:12 +0530

    Commit

--- Monitor Environments with Google Cloud Managed Service for Prometheus Challenge Lab/gsp364.sh
+++ Monitor Environments with Google Cloud Managed Service for Prometheus Challenge Lab/gsp364.sh
@@ -25,7 +25,8 @@ RESET=`tput sgr0`
 
 echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
 
-export ZONE=us-east1-c
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
 gcloud container clusters create gmp-cluster --num-nodes=3 --zone=$ZONE
 
