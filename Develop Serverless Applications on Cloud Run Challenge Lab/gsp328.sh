c021014feb880ae9a83e1e2ae3c60c6d70d6e2d0
QUICK-GCP-LAB <quickgcplab@gmail.com>
2024-12-04 11:14:18 +0530

    Commit

--- Develop Serverless Applications on Cloud Run Challenge Lab/gsp328.sh
+++ Develop Serverless Applications on Cloud Run Challenge Lab/gsp328.sh
@@ -91,7 +91,6 @@ cd ~/pet-theory/lab07/prod-frontend-billing
 gcloud builds submit --tag gcr.io/$DEVSHELL_PROJECT_ID/frontend-prod:0.1
 gcloud run deploy $FRONTEND_PRODUCTION_SERVICE --image gcr.io/$DEVSHELL_PROJECT_ID/frontend-prod:0.1 --quiet
 
-
 echo
 
 # Function to display a random congratulatory message
