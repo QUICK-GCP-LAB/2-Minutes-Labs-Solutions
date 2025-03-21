0f9d40b508edf9c1e31313e02c0f7c39054ed818
QUICK GCP LAB <quickgcplab@gmail.com>
2025-03-21 18:56:21 +0530

    Commit

---
+++ Using BigQuery in the Google Cloud Console/gsp406.sh
@@ -0,0 +1,70 @@
+clear
+
+#!/bin/bash
+# Define color variables
+
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
+
+BOLD=`tput bold`
+RESET=`tput sgr0`
+
+# Array of color codes excluding black and white
+TEXT_COLORS=($RED $GREEN $YELLOW $BLUE $MAGENTA $CYAN)
+BG_COLORS=($BG_RED $BG_GREEN $BG_YELLOW $BG_BLUE $BG_MAGENTA $BG_CYAN)
+
+# Pick random colors
+RANDOM_TEXT_COLOR=${TEXT_COLORS[$RANDOM % ${#TEXT_COLORS[@]}]}
+RANDOM_BG_COLOR=${BG_COLORS[$RANDOM % ${#BG_COLORS[@]}]}
+
+#----------------------------------------------------start--------------------------------------------------#
+
+echo "${RANDOM_BG_COLOR}${RANDOM_TEXT_COLOR}${BOLD}Starting Execution${RESET}"
+
+bq query --use_legacy_sql=false '
+SELECT
+  name, gender,
+  SUM(number) AS total
+FROM
+  `bigquery-public-data.usa_names.usa_1910_2013`
+GROUP BY
+  name, gender
+ORDER BY
+  total DESC
+LIMIT
+  10'
+
+bq --location=us mk --dataset babynames
+
+curl lo 
+
+bq load --source_format=CSV \
+  --autodetect \
+  --skip_leading_rows=0 \
+  babynames.names_2014 yob2014.txt \
+  name:string,gender:string,count:integer
+
+bq query --use_legacy_sql=false '
+SELECT
+  name, count
+FROM
+  `babynames.names_2014`
+WHERE
+  gender = "M"
+ORDER BY count DESC
+LIMIT 5'
