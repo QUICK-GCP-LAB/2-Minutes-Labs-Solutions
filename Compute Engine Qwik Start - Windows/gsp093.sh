e35411bb5607d830bcddb60eb88b37c602248873
QUICK GCP LAB <quickgcplab@gmail.com>
2025-04-06 12:06:34 +0530

    Commit

--- Compute Engine Qwik Start - Windows/gsp093.sh
+++ Compute Engine Qwik Start - Windows/gsp093.sh
@@ -46,7 +46,8 @@ echo "${BOLD}${GREEN}Creating GCP Windows instance 'quickgcplab'${RESET}"
 gcloud compute instances create quickgcplab --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=quicklab,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20230913,mode=rw,size=50,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
 
 # Step 3: Wait for instance to be ready
-echo "${BOLD}${YELLOW}Waiting for instance setup to finish and resetting Windows password${RESET}"
+echo
+echo "${BOLD}${CYAN}Waiting for instance setup to finish and resetting Windows password${RESET}"
 INSTANCE_NAME="quickgcplab"
 wait_and_reset_windows_password() {
   local INSTANCE_NAME=$1
