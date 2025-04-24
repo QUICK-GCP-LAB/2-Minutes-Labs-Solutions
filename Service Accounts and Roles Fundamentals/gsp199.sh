f718243d609b7eb561721da0601cabd40f8954fe
QUICK GCP LAB <quickgcplab@gmail.com>
2025-04-24 15:36:37 +0530

    Commit

--- Service Accounts and Roles Fundamentals/gsp199.sh
+++ Service Accounts and Roles Fundamentals/gsp199.sh
@@ -79,23 +79,61 @@ mode=rw,size=10,type=pd-balanced \
   --labels=goog-ec-src=vm_add-gcloud \
   --reservation-affinity=any
 
-cat > prepare_disk.sh <<'EOF_END'
-export PROJECT_ID=$(gcloud config get-value project)
+export ZONE=$(gcloud compute project-info describe \
+--format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
-sudo apt-get update
-sudo apt-get install -y git python3-pip
-pip3 install --upgrade pip
-pip3 install google-cloud-bigquery
-pip3 install pyarrow
-pip3 install pandas
-pip3 install db-dtypes
+gcloud iam service-accounts create my-sa-123 --display-name "my service account"
 
-echo "
+sleep 10
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+    --member serviceAccount:my-sa-123@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com --role roles/editor
+
+gcloud iam service-accounts create bigquery-qwiklab \
+  --description="Service account for BigQuery Qwiklabs" \
+  --display-name="bigquery-qwiklab"
+
+sleep 10
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/bigquery.dataViewer"
+
+gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
+  --member="serviceAccount:bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+  --role="roles/bigquery.user"
+
+gcloud compute instances create bigquery-instance \
+  --project=$DEVSHELL_PROJECT_ID \
+  --zone=$ZONE \
+  --machine-type=e2-medium \
+  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+  --metadata=enable-oslogin=true \
+  --maintenance-policy=MIGRATE \
+  --provisioning-model=STANDARD \
+  --service-account=bigquery-qwiklab@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com \
+  --scopes=https://www.googleapis.com/auth/cloud-platform \
+  --create-disk=auto-delete=yes,boot=yes,device-name=bigquery-instance,\
+image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,\
+mode=rw,size=10,type=pd-balanced \
+  --no-shielded-secure-boot \
+  --shielded-vtpm \
+  --shielded-integrity-monitoring \
+  --labels=goog-ec-src=vm_add-gcloud \
+  --reservation-affinity=any
+
+gcloud compute ssh bigquery-instance --zone=$ZONE --command='
+  sudo apt-get update &&
+  sudo apt-get install -y git python3-pip &&
+  pip3 install --upgrade pip &&
+  pip3 install google-cloud-bigquery pyarrow pandas db-dtypes &&
+  export PROJECT_ID=$(gcloud config get-value project) &&
+  echo "
 from google.auth import compute_engine
 from google.cloud import bigquery
 credentials = compute_engine.Credentials(
-    service_account_email='bigquery-qwiklab@$PROJECT_ID.iam.gserviceaccount.com')
-query = '''
+    service_account_email=\"bigquery-qwiklab@${PROJECT_ID}.iam.gserviceaccount.com\")
+query = \"\"\"
 SELECT
   year,
   COUNT(1) as num_babies
@@ -105,18 +143,12 @@ WHERE
   year > 2000
 GROUP BY
   year
-'''
-client = bigquery.Client(
-    project='$PROJECT_ID',
-    credentials=credentials)
+\"\"\"
+client = bigquery.Client(project=\"${PROJECT_ID}\", credentials=credentials)
 print(client.query(query).to_dataframe())
-" > query.py
-EOF_END
-
-
-gcloud compute scp prepare_disk.sh bigquery-instance:/tmp/prepare_disk.sh --zone=$ZONE
-
-gcloud compute ssh bigquery-instance --zone=$ZONE --command="bash /tmp/prepare_disk.sh"
+" > query.py &&
+  python3 query.py
+'
 
 echo
 
